fe20ab3a5782e3d4ed9bdfc936b9fa550a4374ff
xunoaib <xunoaib@gmail.com>
2021-12-01 16:06:48 -0500

    clean up day 1

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
+import sys
+
+def count_increases(lst):
+    count = 0
+    for i in range(len(lst)-1):
+        if lst[i] < lst[i+1]:
+            count += 1
+    return count
 
 def main():
-    lines = open('day1.in').readlines()
+    lines = sys.stdin.readlines()
     depths = list(map(int, lines))
 
-    # part 1
-    count = 0
-    for i in range(1, len(depths)):
-        if depths[i-1] < depths[i]:
-            count += 1
-    print(count)
+    part1 = count_increases(depths)
+    print('part1:', part1)
+    assert part1 == 1696
 
-    # part 2
     sums = []
     for i in range(len(depths)-2):
         sums.append(sum(depths[i:i+3]))
 
-    count = 0
-    for i in range(1, len(sums)):
-        if sums[i-1] < sums[i]:
-            count += 1
-    print(count)
+    part2 = count_increases(sums)
+    print('part2:', part2)
+    assert part2 == 1737
 
 if __name__ == "__main__":
     main()
