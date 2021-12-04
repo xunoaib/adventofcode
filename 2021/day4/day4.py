90ca247acbe047d20a23ab3d60ca868c73ba29ba
xunoaib <xunoaib@gmail.com>
2021-12-04 05:22:36 -0500

    Day 4 (revised)

--- 2021/day4/day4.py
+++ 2021/day4/day4.py
@@ -4,41 +4,44 @@ import re
 import sys
 
 def has_won(board):
-    ''' Look for 5 marked cells in any row or column '''
-    return [None]*5 in board or (None,)*5 in list(zip(*board))
+    '''Look for 5 marked cells in any row or column'''
+    return [None] * 5 in board or (None,) * 5 in zip(*board)
 
 def mark_board(board, num):
-    ''' Update a bingo board with the given number. Returns whether it was a winning move '''
+    '''Update a bingo board with the given number. Returns whether it was a winning move'''
     for r, row in enumerate(board):
         board[r] = [None if n == num else n for n in row]
     return has_won(board)
 
 def calc_score(board, num):
-    ''' Return the sum of all unmarked cells multiplied by the winning bingo number '''
-    return sum([v for row in board for v in row if v is not None]) * num
+    '''Return the sum of all unmarked cells multiplied by the winning bingo number'''
+    return sum(v for row in board for v in row if v is not None) * num
 
 def part1(numbers, boards):
-    ''' Play rounds until one board wins. Returns the score of the winning board '''
+    '''Play rounds until one board wins. Returns the score of the winning board'''
     boards = copy.deepcopy(boards)
     for num in numbers:
-        for i, board in enumerate(boards):
+        for board in boards:
             if mark_board(board, num):
                 return calc_score(board, num)
 
 def part2(numbers, boards):
-    ''' Play rounds until all boards have won. Returns the score of the last winning board '''
+    '''Play rounds until all boards have won. Returns the score of the last winning board'''
     boards = copy.deepcopy(boards)
     for num in numbers:
-        for i, board in list(enumerate(boards)):
+        for board in list(boards):
             if mark_board(board, num):
                 boards.remove(board)
-                if len(boards) == 0:
+                if not boards:
                     return calc_score(board, num)
 
 def main():
     numbers = list(map(int, sys.stdin.readline().split(',')))
     groups = sys.stdin.read().strip().split('\n\n')
-    boards = [[list(map(int, re.split(r'\s+', line.strip()))) for line in group.split('\n')] for group in groups]
+    boards = [
+        [list(map(int, re.split(r'\s+', line.strip()))) for line in group.split('\n')]
+        for group in groups
+    ]
 
     ans1 = part1(numbers, boards)
     ans2 = part2(numbers, boards)
