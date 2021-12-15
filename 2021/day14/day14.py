f7f7a5273d71e28f09b1a9a569dbe235b19af046
xunoaib <xunoaib@gmail.com>
2021-12-15 00:00:01 -0500

    Day 14, 2021 revised

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -3,23 +3,7 @@ import sys
 from collections import Counter
 from functools import cache
 
-
-def replace(pattern, rules):
-    s = ''
-    for i in range(len(pattern) - 1):
-        pair = pattern[i:i + 2]
-        s += pair[0]
-        if rule := rules.get(pair):
-            s += rule
-    return s + pair[1]
-
-def part1(pattern, rules):
-    for _ in range(10):
-        pattern = replace(pattern, rules)
-    counts = Counter(pattern)
-    return max(counts.values()) - min(counts.values())
-
-def part2(pattern, rules):
+def count_letters(pattern, rules, depth):
     @cache
     def recurse(pair, depth=0):
         """Returns the number of occurrences of each character after 'depth' iterations"""
@@ -37,8 +21,7 @@ def part2(pattern, rules):
     # split large string into character pairs and gather character counts for each
     counts = Counter()
     for i in range(len(pattern) - 1):
-        pair = pattern[i:i + 2]
-        subcounts = recurse(pair, 40)
+        subcounts = recurse(pattern[i:i + 2], depth)
         for k, v in subcounts.items():
             counts[k] += v
 
@@ -52,10 +35,10 @@ def main():
     pattern = sys.stdin.readline().strip()
     rules = dict(line.split(' -> ') for line in sys.stdin.read().strip().split('\n'))
 
-    ans1 = part1(pattern, rules)
+    ans1 = count_letters(pattern, rules, 10)
     print('part1:', ans1)
 
-    ans2 = part2(pattern, rules)
+    ans2 = count_letters(pattern, rules, 40)
     print('part2:', ans2)
 
     assert ans1 == 5656
