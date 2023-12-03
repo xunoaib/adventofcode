c0af8831216aaa59877df07c11dbc9a9814e854e
xunoaib <xunoaib@gmail.com>
2023-12-03 15:46:41 -0500

    2023 Day 1 tweaks

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -20,15 +20,15 @@ namestodigit = {name: str(i + 1) for i, name in enumerate(digitnames)}
 
 def score(line, pattern):
     found = re.findall(f'(?={pattern})', line)
-    digits = ''.join([namestodigit.get(d, d) for d in found])
+    digits = ''.join(namestodigit.get(d, d) for d in found)
     return int(digits[0] + digits[-1])
 
 
 def main():
     lines = sys.stdin.read().strip().lower().split('\n')
 
-    pattern1 = '(' + r'|'.join(string.digits) + ')'
-    pattern2 = '(' + r'|'.join(list(string.digits) + digitnames) + ')'
+    pattern1 = '(' + '|'.join(string.digits) + ')'
+    pattern2 = '(' + '|'.join(list(string.digits) + digitnames) + ')'
 
     ans1 = sum(score(line, pattern1) for line in lines)
     ans2 = sum(score(line, pattern2) for line in lines)
