efaa927e31b405a2f31a99a24e1b69cfc911f0b3
xunoaib <xunoaib@gmail.com>
2023-12-12 02:57:06 -0500

    2023 Day 12 Part 2

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -1,34 +1,62 @@
 #!/usr/bin/env python3
-import re
 import sys
-from itertools import product
+from functools import cache
 
-def isvalid(line, nums):
-    return nums == [len(g) for g in re.split(r'\.+', line.strip('.'))]
+chars: list = []
+nums: list = []
 
 
-def solve1(line, nums):
-    chars = list(line)
-    qs = [i for i, ch in enumerate(line) if ch == '?']
-    count = 0
-    for perm in product('.#', repeat=len(qs)):
-        for i, ch in zip(qs, perm):
-            chars[i] = ch
-        if isvalid(''.join(chars), nums):
-            count += 1
-    return count
+@cache
+def solve(ch=None, idx=0, nidx=0, groupsize=0):
+    if idx >= len(chars):
+        return int(nidx >= len(nums))
+
+    ch = chars[idx]
+    if ch == '#':
+        groupsize += 1
+        if nidx >= len(nums) or groupsize > nums[nidx]:
+            return 0
+    elif ch == '.':
+        if chars[idx - 1] == '#':
+            if groupsize != nums[nidx]:
+                return 0
+            groupsize = 0
+            nidx += 1
+    elif ch == '?':
+        chars[idx] = '.'
+        a = solve('.', idx, nidx, groupsize)
+        chars[idx] = '#'
+        b = solve('#', idx, nidx, groupsize)
+        chars[idx] = '?'
+        return a + b
+
+    return solve(ch, idx + 1, nidx, groupsize)
 
 
 def main():
+    global nums, chars
     lines = sys.stdin.read().strip().split('\n')
 
-    ans1 = 0
+    ans1 = ans2 = 0
     for line in lines:
-        line, nums = line.split(' ')
-        nums = list(map(int, nums.split(',')))
-        ans1 += solve1(line, nums)
+        line, ns = line.split(' ')
+        nums = list(map(int, ns.split(',')))
+        chars = list('.' + line + '.')
+        solve.cache_clear()
+        ans1 += solve()
+
+        line = '?'.join([line] * 5)
+        nums *= 5
+        chars = list('.' + line + '.')
+        solve.cache_clear()
+        ans2 += solve()
 
     print('part1:', ans1)
+    print('part2:', ans2)
+
+    assert ans1 == 8180
+    assert ans2 == 620189727003627
+
 
 if __name__ == '__main__':
     main()
