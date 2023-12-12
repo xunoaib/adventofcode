191b75dc78f3cbb3108c410833d7a9ba962911a7
xunoaib <xunoaib@gmail.com>
2023-12-12 03:33:56 -0500

    2023 Day 12 Part 2 tweaks

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -2,54 +2,48 @@
 import sys
 from functools import cache
 
-chars: list = []
-nums: list = []
 
+def solve(line, nums):
+    chars = list('.' + line + '.')
 
-@cache
-def solve(ch=None, idx=0, nidx=0, groupsize=0):
-    if idx >= len(chars):
-        return int(nidx >= len(nums))
+    @cache
+    def _solve(ch=None, idx=0, nidx=0, groupsize=0):
+        if idx >= len(chars):
+            return int(nidx >= len(nums))
 
-    ch = chars[idx]
-    if ch == '#':
-        groupsize += 1
-        if nidx >= len(nums) or groupsize > nums[nidx]:
-            return 0
-    elif ch == '.':
-        if chars[idx - 1] == '#':
-            if groupsize != nums[nidx]:
-                return 0
-            groupsize = 0
-            nidx += 1
-    elif ch == '?':
-        chars[idx] = '.'
-        a = solve('.', idx, nidx, groupsize)
-        chars[idx] = '#'
-        b = solve('#', idx, nidx, groupsize)
-        chars[idx] = '?'
-        return a + b
+        match chars[idx]:
+            case '#':
+                groupsize += 1
+                if nidx >= len(nums) or groupsize > nums[nidx]:
+                    return 0
+            case '.':
+                if chars[idx - 1] == '#':
+                    if groupsize != nums[nidx]:
+                        return 0
+                    groupsize = 0
+                    nidx += 1
+            case '?':
+                chars[idx] = '.'
+                a = _solve('.', idx, nidx, groupsize)
+                chars[idx] = '#'
+                b = _solve('#', idx, nidx, groupsize)
+                chars[idx] = '?'
+                return a + b
 
-    return solve(ch, idx + 1, nidx, groupsize)
+        return _solve(chars[idx], idx + 1, nidx, groupsize)
+
+    return _solve()
 
 
 def main():
-    global nums, chars
     lines = sys.stdin.read().strip().split('\n')
-
     ans1 = ans2 = 0
+
     for line in lines:
         line, ns = line.split(' ')
         nums = list(map(int, ns.split(',')))
-        chars = list('.' + line + '.')
-        solve.cache_clear()
-        ans1 += solve()
-
-        line = '?'.join([line] * 5)
-        nums *= 5
-        chars = list('.' + line + '.')
-        solve.cache_clear()
-        ans2 += solve()
+        ans1 += solve(line, nums)
+        ans2 += solve('?'.join([line] * 5), nums * 5)
 
     print('part1:', ans1)
     print('part2:', ans2)
