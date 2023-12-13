18e79e5cb0b2271dde69e3a8c0fdb587a125c59a
xunoaib <xunoaib@gmail.com>
2023-12-13 02:07:21 -0500

    2023 Day 13 Part 2

--- 2023/day13/day13.py
+++ 2023/day13/day13.py
@@ -2,37 +2,37 @@
 import sys
 
 
-def vreflect(lines, r):
-    upper, lower = lines[:r], lines[r:]
-    upper = upper[::-1]
-    return all(a == b for a, b in zip(upper, lower))
+def vdiffcount(lines, r):
+    upper, lower = lines[:r][::-1], lines[r:]
+    return sum(1 for a, b in zip(upper, lower) for x, y in zip(a, b) if x != y)
 
 
-def vert_sym(lines):
+def findvert(lines, diffcount):
     for r in range(1, len(lines)):
-        if vreflect(lines, r):
+        if vdiffcount(lines, r) == diffcount:
             return r
+    return 0
 
 
-def horiz_sym(lines):
+def findhoriz(lines, diffcount):
     lines = [''.join(line[c] for line in lines) for c in range(len(lines[0]))]
-    return vert_sym(lines)
+    return findvert(lines, diffcount)
 
 
 def main():
     patterns = sys.stdin.read().strip().split('\n\n')
-    a1 = 0
+    a1 = a2 = 0
 
-    for pat in patterns:
-        lines = pat.split('\n')
-
-        if v := vert_sym(lines):
-            a1 += v * 100
-
-        if h := horiz_sym(lines):
-            a1 += h
+    for p in patterns:
+        lines = p.split('\n')
+        a1 += findhoriz(lines, 0) + findvert(lines, 0) * 100
+        a2 += findhoriz(lines, 1) + findvert(lines, 1) * 100
 
     print('part1:', a1)
+    print('part2:', a2)
+
+    assert a1 == 35232
+    assert a2 == 37982
 
 
 if __name__ == '__main__':
