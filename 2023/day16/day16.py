e46c8d31ed84402d9b056c26cb32157305957e07
xunoaib <xunoaib@gmail.com>
2023-12-16 01:35:42 -0500

    2023 Day 16 Part 2 tweaks

--- 2023/day16/day16.py
+++ 2023/day16/day16.py
@@ -1,59 +1,62 @@
 #!/usr/bin/env python3
 import sys
 
-DIRS = L, R, U, D = (0,-1), (0,1), (-1,0), (1,0)
+DIRS = L, R, U, D = (0, -1), (0, 1), (-1, 0), (1, 0)
 
 states = set()
 
+
 def nextdirs(ch, curdir):
     if ch == '.':
-        return (curdir,)
+        return (curdir, )
     if ch == '-':
         if curdir in (L, R):
-            return (curdir,)
+            return (curdir, )
         return (L, R)
     if ch == '|':
         if curdir in (U, D):
-            return (curdir,)
+            return (curdir, )
         return (U, D)
     if ch == '/':
         return {
-            L: (D,),
-            U: (R,),
-            R: (U,),
-            D: (L,),
+            L: (D, ),
+            U: (R, ),
+            R: (U, ),
+            D: (L, ),
         }[curdir]
     if ch == '\\':
         return {
-            L: (U,),
-            U: (L,),
-            R: (D,),
-            D: (R,),
+            L: (U, ),
+            U: (L, ),
+            R: (D, ),
+            D: (R, ),
         }[curdir]
+    return None, None
+
 
 def part1(grid, visited, r, c, curdir):
-    state = (r,c, curdir)
+    state = (r, c, curdir)
     if state in states:
         return
     states.add(state)
 
-    visited.add((r,c))
+    visited.add((r, c))
     while True:
-        ndirs = nextdirs(grid.get((r,c)), curdir)
+        ndirs = nextdirs(grid.get((r, c)), curdir)
         if len(ndirs) != 1:
             break
         curdir = ndirs[0]
         r += curdir[0]
         c += curdir[1]
-        if (r,c) not in grid:
+        if (r, c) not in grid:
             return
-        visited.add((r,c))
+        visited.add((r, c))
 
     for ndir in ndirs:
-        roff, coff = ndir
-        npos = nr, nc = r+roff, c+coff
+        npos = r + ndir[0], c + ndir[1]
         if npos in grid:
-            part1(grid, visited, nr, nc, ndir)
+            part1(grid, visited, *npos, ndir)
+
 
 def main():
     lines = sys.stdin.read().strip().split('\n')
@@ -64,34 +67,29 @@ def main():
             g[r, c] = ch
 
     visited = set()
-    r,c = pos = (0, 0)
-    roff, coff = curdir = R
-    part1(g,visited,r,c,curdir)
+    part1(g, visited, 0, 0, R)
     a1 = len(visited)
     print('part1:', a1)
 
-    rs, cs = zip(*g)
-    maxr = max(rs)
-    maxc = max(cs)
+    maxr, maxc = [max(v) for v in zip(*g)]
 
-    points = [(r, 0) for r in range(maxr+1)]
-    points += [(r, maxc) for r in range(maxr+1)]
-    points += [(0, c) for c in range(maxc+1)]
-    points += [(maxr, c) for c in range(maxc+1)]
-    points = set(points)
+    points = []
+    points += [(r, 0, R) for r in range(maxr + 1)]
+    points += [(r, maxc, L) for r in range(maxr + 1)]
+    points += [(0, c, D) for c in range(maxc + 1)]
+    points += [(maxr, c, U) for c in range(maxc + 1)]
 
     a2 = 0
-    for d in DIRS:
-        for r,c in points:
-            states.clear()
-            visited = set()
-            curdir = R
-            part1(g,visited,r,c,d)
-            a2 = max(a2, len(visited))
+    for r, c, d in points:
+        states.clear()
+        visited = set()
+        part1(g, visited, r, c, d)
+        a2 = max(a2, len(visited))
     print('part2:', a2)
 
     assert a1 == 6622
     assert a2 == 7130
+
 
 if __name__ == '__main__':
     main()
