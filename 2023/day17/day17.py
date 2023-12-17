800e268153d7d781a7594ea11eeaed46f4d5ab7b
xunoaib <xunoaib@gmail.com>
2023-12-17 03:40:49 -0500

    2023 Day 17 Part 2

--- 2023/day17/day17.py
+++ 2023/day17/day17.py
@@ -1,92 +1,89 @@
 #!/usr/bin/env python3
-# import copy
-# import re
-# import numpy as np
-from collections import Counter
-from functools import cache
-from itertools import product
-from heapq import heappop, heappush
 import sys
+from collections import Counter
+from heapq import heappop, heappush
 
 DIRS = U, D, L, R = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
-def neighbors(grid, current):
+def neighbors1(grid, gscores, current):
     r, c, lastdir, dircount = current
-    # for roff, coff in product([-1, 0, 1], repeat=2):
     for roff, coff in DIRS:
-        if roff == coff == 0 or 0 not in (roff, coff):
-            continue
-        if lastdir == (-roff, -coff):
-            continue
         nr = r + roff
         nc = c + coff
         if (nr, nc) not in grid:
             continue
-        newdircount = (dircount + 1) if lastdir == (roff, coff) else 1
-        if newdircount <= 3:
-            yield nr, nc, (roff, coff), newdircount
+        if lastdir == (-roff, -coff):
+            continue
+
+        ngscore = gscores[current] + grid[nr, nc]
+        if (roff, coff) != lastdir:
+            ndircount = 1  # changed direction
+        elif dircount < 3:
+            ndircount = dircount + 1
+        else:
+            continue  # exceeded 3 blocks in one direction
+
+        yield ngscore, (nr, nc, (roff, coff), ndircount)
 
 
-def part1(grid):
+def neighbors2(grid, gscores, current):
+    r, c, lastdir, dircount = current
+    for roff, coff in DIRS:
+        nr = r + roff
+        nc = c + coff
+        if (nr, nc) not in grid:
+            continue
+        if lastdir == (-roff, -coff):
+            continue
+
+        ngscore = gscores[current] + grid[nr, nc]
+        if (roff, coff) != lastdir:
+            ndircount = 4  # always walk 4 blocks in a new direction
+            for _ in range(3):
+                nr += roff
+                nc += coff
+                ngscore += grid.get((nr, nc), 0)
+            if (nr, nc) not in grid:
+                continue
+        elif dircount < 10:
+            ndircount = dircount + 1
+        else:
+            continue  # >10 consecutive blocks (invalid)
+
+        yield ngscore, (nr, nc, (roff, coff), ndircount)
+
+
+def solve(grid, neighbors):
 
     def h(row, col):
         return abs(maxr - row) + abs(maxc - col)
 
-    def reconstruct_path(node):
-        path = []
-        while node:
-            path.append(node)
-            node = cameFrom[node]
-        return path
-
-    def print_path(path):
-        g = grid.copy()
-        while path:
-            r, c, d, _ = path.pop()
-            g[r,c] = f'\033[91;1m{g[r,c]}\033[0m'
-
-        for r in range(maxr+1):
-            for c in range(maxc+1):
-                print(g[r,c], end='')
-            print()
-        print()
-
     maxr = max(r for r, _ in grid)
     maxc = max(c for _, c in grid)
-    goal = (maxr, maxc)
 
-    gscores = {}
-    fscores = {}
-    cameFrom = {}
-    counter = Counter()
-
-    pos = (0, 0)
-    current = (*pos, None, 0)
-    gscores[current] = 0
-    fscores[current] = gscores[current] + h(*pos)
-    counter[current] += 1
-    cameFrom[current] = None
-
-    q = [(fscores[current], (*pos, None, 0))]
+    r = c = 0
+    current = (r, c, None, 0)
+    gscores = {current: 0}
+    fscores = {current: h(r, c)}
+    cameFrom = {current: None}
+    counter = Counter({current: 1})
+    q = [(fscores[current], current)]
 
     while q:
-        _, current = _, (r, c, _, _) = heappop(q)
+        _, current = heappop(q)
+        r, c = current[0:2]
         current = tuple(current)
         counter[current] -= 1
 
-        if (r, c) == goal:
-            path = reconstruct_path(current)
-            print_path(path)
+        if (r, c) == (maxr, maxc):
             return gscores[current]
 
-        for neighbor in neighbors(grid, current):
-            npos = neighbor[0:2]
-            ngscore = gscores[current] + grid[npos]
+        for ngscore, neighbor in neighbors(grid, gscores, current):
             if ngscore < gscores.get(neighbor, sys.maxsize):
                 cameFrom[neighbor] = current
                 gscores[neighbor] = ngscore
-                fscores[neighbor] = ngscore + h(*npos)
+                fscores[neighbor] = ngscore + h(*neighbor[0:2])
                 if counter[neighbor] == 0:
                     counter[neighbor] += 1
                     heappush(q, (fscores[neighbor], neighbor))
@@ -101,8 +98,15 @@ def main():
         for c, ch in enumerate(line)
     }
 
-    a1 = part1(grid)
+    a1 = solve(grid, neighbors1)
     print('part1:', a1)
+
+    a2 = solve(grid, neighbors2)
+    print('part2:', a2)
+
+    assert a1 == 907
+    assert a2 == 1057
+
 
 if __name__ == '__main__':
     main()
