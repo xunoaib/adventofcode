dfe33702ba580e7d74cb5122a8c9e02c15f78044
xunoaib <xunoaib@gmail.com>
2023-12-18 03:55:57 -0500

    2023 Day 18 Part 2

--- 2023/day18/day18.py
+++ 2023/day18/day18.py
@@ -1,106 +1,52 @@
 #!/usr/bin/env python3
-import enum
 import re
 import sys
+from itertools import pairwise
 
-DIRS = U, D, L, R = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-cdirs = {'U': U, 'L': L, 'R': R, 'D': D}
-
-
-def neighbors(r, c):
-    for roff, coff in DIRS:
-        yield r + roff, c + coff
+DIRDICT = {'U': (-1, 0), 'L': (0, -1), 'R': (0, 1), 'D': (1, 0)}
 
 
-def getbounds(s):
-    rs, cs = zip(*s)
-    minr, maxr = min(rs), max(rs)
-    minc, maxc = min(cs), max(cs)
-    return minr, maxr, minc, maxc
+def parseline1(line):
+    dirch, count, _ = re.search(r'(.*) (.*) \(#(.*)\)', line).groups()
+    roff, coff = DIRDICT[dirch]
+    return roff, coff, int(count)
 
 
-def getpool(border, r, c):
-    minr, maxr, minc, maxc = getbounds(border)
-
-    minr -= 1
-    minc -= 1
-    maxr += 1
-    maxc += 1
-
-    assert r, c not in border
-    q = [(r, c)]
-    pool = {(r, c)}
-    seen = {(r, c)}
-
-    while q:
-        r, c = q.pop()
-        if (r, c) in border:
-            continue
-        for nr, nc in neighbors(r, c):
-            if (nr, nc) in seen or (nr, nc) in pool:
-                continue
-            seen.add((nr, nc))
-            if minr <= nr <= maxr and minc <= nc <= maxc and (
-                    nr, nc) not in border:
-                pool.add((nr, nc))
-                q.append((nr, nc))
-    return pool
+def parseline2(line):
+    _, _, color = re.search(r'(.*) (.*) \(#(.*)\)', line).groups()
+    roff, coff = DIRDICT['RDLU'[int(color[-1])]]
+    return roff, coff, int(color[:-1], 16)
 
 
-def print_lagoon(lagoon):
-    minr, maxr, minc, maxc = getbounds(lagoon)
-    for r in range(minr, maxr + 1):
-        for c in range(minc, maxc + 1):
-            ch = '#' if (r, c) in lagoon else '.'
-            print(ch, end='')
-        print()
+def solve(lines, parseline):
+    r = c = 0
+    points = [(r, c)]
+    perimeter = 0
+
+    for line in lines:
+        roff, coff, count = parseline(line)
+        r += roff * count
+        c += coff * count
+        points.append((r, c))
+        perimeter += count
+
+    # shoelace formula
+    area = 0.5 * abs(
+        sum((r1 + r2) * (c1 - c2) for (r1, c1), (r2, c2) in pairwise(points)))
+    return int(area) + perimeter // 2 + 1
 
 
 def main():
     lines = sys.stdin.read().strip().split('\n')
 
-    r, c = 0, 0
-    lagoon = {(r, c)}
+    a1 = solve(lines, parseline1)
+    a2 = solve(lines, parseline2)
 
-    for line in lines:
-        d, count, h = re.search(r'(.*) (.*) \(#(.*)\)', line).groups()
-        roff, coff = cdirs[d]
-        for _ in range(int(count)):
-            r, c = r + roff, c + coff
-            lagoon.add((r, c))
+    print('part1:', a1)
+    print('part2:', a2)
 
-    minr, maxr, minc, maxc = getbounds(lagoon)
-
-    # find all open spots adjacent to the border
-    adjopen = set()
-    for r, c in lagoon:
-        for nr, nc in neighbors(r, c):
-            if (nr, nc) in lagoon and (nr, nc) not in adjopen:
-                continue
-            if minr <= nr <= maxr and minc <= nc <= maxc:
-                adjopen.add((nr, nc))
-
-    # find one pool
-    pools = []
-    adjopen = list(adjopen)
-    while adjopen:
-        r, c = adjopen.pop()
-        newpool = getpool(lagoon, r, c)
-        for i, p in enumerate(pools):
-            if newpool & p:
-                pools[i] |= newpool
-                break
-        else:
-            pools.append(newpool)
-        adjopen = list(set(adjopen) - newpool)
-
-    for p in pools:
-        if (minr - 1, minc - 1) not in p:
-            print('part1:', len(p) + len(lagoon))
-
-    # assert a1 == 0
-    # assert a2 == 0
+    assert a1 == 49578
+    assert a2 == 52885384955882
 
 
 if __name__ == '__main__':
