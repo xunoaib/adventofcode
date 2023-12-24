5fdd2b00879ca0b6143179f213b28f4a6a541216
xunoaib <xunoaib@gmail.com>
2023-12-24 02:24:17 -0500

    2023 Day 24 Part 2

--- 2023/day24/day24.py
+++ 2023/day24/day24.py
@@ -3,25 +3,18 @@ import re
 import sys
 from itertools import combinations
 
+from z3 import Real, Reals, Solver, sat
 
-def main():
-    lines = sys.stdin.read().strip().split('\n')
+VMIN, VMAX = (200000000000000, 400000000000000)
 
-    stones = []
-    for line in lines:
-        if m := re.match(r'(\d+), (\d+), (\d+) @ (.*), (.*), (.*)', line):
-            stones.append(list(map(int, m.groups())))
 
-    coeffs = []
+def part1(stones):
+    coeffs = []  # ax + bx + c
     for stone in stones:
         px, py, pz, vx, vy, vz = stone
         m = vy / vx
         b = py - m * px
-        coeff = A, B, C = (m, -1, b)
-        coeffs.append(coeff)
-
-    # vmin, vmax = (7, 27)
-    vmin, vmax = (200000000000000, 400000000000000)
+        coeffs.append((m, -1, b))
 
     crossed = set()
     for (idx_a, coeffs_a), (idx_b, coeffs_b) in combinations(enumerate(coeffs),
@@ -33,23 +26,55 @@ def main():
             yint = a1 * (c2 - c1) / (a1 - a2) + c1
 
             # ensure the collision happens in the future
-            px_a, _, _, vx_a, _, _ = stones[idx_a]
-            px_b, _, _, vx_b, _, _ = stones[idx_b]
+            px_a, py_a, pz_a, vx_a, vy_a, vz_a = stones[idx_a]
+            px_b, py_b, pz_b, vx_b, vy_b, vz_b = stones[idx_b]
             t_a = (xint - px_a) / vx_a
             t_b = (xint - px_b) / vx_b
+
             if t_a < 0 or t_b < 0:
                 continue
 
-            if vmin <= xint <= vmax and vmin <= yint <= vmax:
+            if VMIN <= xint <= VMAX and VMIN <= yint <= VMAX:
                 crossed |= {(idx_a, idx_b)}
 
         except ZeroDivisionError:
-            pass  # no intersection
+            pass  # parallel
 
-    a1 = len(crossed)
-    print('part1:', )
+    return len(crossed)
+
+
+def part2(stones):
+    s = Solver()
+    gpx, gpy, gpz, gvx, gvy, gvz = Reals('gpx gpy gpz gvx gvy gvz')
+
+    for i, stone in enumerate(stones):
+        px, py, pz, vx, vy, vz = stone
+        t = Real(f't{i}')
+        s.add(gpx + t * gvx == px + t * vx)
+        s.add(gpy + t * gvy == py + t * vy)
+        s.add(gpz + t * gvz == pz + t * vz)
+
+    assert s.check() == sat
+    m = s.model()
+    return m.eval(gpx + gpy + gpz)
+
+
+def main():
+    lines = sys.stdin.read().strip().split('\n')
+
+    stones = []
+    for line in lines:
+        if m := re.match(r'(\d+), (\d+), (\d+) @ (.*), (.*), (.*)', line):
+            stones.append(list(map(int, m.groups())))
+
+    a1 = part1(stones)
+    print('part1:', a1)
+
+    a2 = part2(stones)
+    print('part2:', a2)
 
     assert a1 == 16665
+    assert a2 == 769840447420960
 
 
 if __name__ == '__main__':
