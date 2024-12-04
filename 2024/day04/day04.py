7e66b448cecc13d0bd0438a1a6b62731793fc1f4
xunoaib <xunoaib@gmail.com>
2024-12-04 00:10:42 -0500

    Day 4, 2024: Part 1

---
+++ 2024/day04/day04.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+from itertools import product
+
+dirs = [(r,c) for r,c in product([-1,0,1], [-1,0,1]) if r or c]
+
+def count(r, c):
+    total = 0
+    for roff, coff in dirs:
+        R, C = r, c
+        for ch in 'XMAS':
+            if d.get((R,C)) != ch:
+                break
+            R += roff
+            C += coff
+        else:
+            total += 1
+    return total
+
+g = []
+for line in sys.stdin:
+    line = line.strip()
+    g.append(line)
+
+d = {}
+for r, row in enumerate(g):
+    for c, ch in enumerate(row):
+        d[r,c] = ch
+
+p1 = 0
+for (r,c), ch in d.items():
+    if ch == 'X':
+        p1 += count(r,c)
+
+print('part1:', p1)
