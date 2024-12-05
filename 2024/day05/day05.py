b363bf718e0b045bae8461e656da5ded52483542
xunoaib <xunoaib@gmail.com>
2024-12-05 00:30:48 -0500

    Day 5, 2024

---
+++ 2024/day05/day05.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import sys
+
+s1, s2  = sys.stdin.read().strip().split('\n\n')
+
+rules = [tuple(map(int, line.split('|'))) for line in s1.split('\n')]
+updates = [tuple(map(int, line.split(','))) for line in s2.split('\n')]
+
+def find_smallest(update):
+    '''Find the only number which has no other numbers come before it'''
+
+    lessthan = {v: set() for v in update}
+
+    for x,y in rules:
+        if x in update and y in update:
+            lessthan[y].add(x)
+
+    for v, lessthanv in lessthan.items():
+        if not lessthanv:
+            return v
+
+
+def fix_order(update):
+    fixed = []
+    left = list(update)
+
+    while left:
+        s = find_smallest(left)
+        fixed.append(s)
+        left.remove(s)
+
+    return fixed
+
+
+def is_valid(update, x, y):
+    if x in update and y in update:
+        return update.index(x) < update.index(y)
+    return True
+
+p1 = p2 = 0
+
+for update in updates:
+    if all(is_valid(update, x,y) for x,y in rules):
+        p1 += update[len(update)//2]
+    else:
+        update = fix_order(update)
+        p2 += update[len(update)//2]
+
+print('part1:', p1)
+print('part2:', p2)
