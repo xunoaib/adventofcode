a882c29fcdf6843ea3bfdebfa21f3ef906d2c391
xunoaib <xunoaib@gmail.com>
2024-12-06 00:41:51 -0500

    Day 6, 2024 - Part 2 Tweaks

--- 2024/day06/day06.py
+++ 2024/day06/day06.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-import time
+
+DIRS = (-1,0), (0,1), (1,0), (0,-1)
 
 lines = sys.stdin.read().strip().split('\n')
-
-U,R,D,L = DIRS  = (-1,0), (0,1), (1,0),(0,-1)
-d = 0
 
 grid = {
     (r, c): ch
@@ -14,63 +12,33 @@ grid = {
     for c, ch in enumerate(line)
 }
 
-walls = {p for p,ch in grid.items() if ch == '#'}
 start = next(p for p,ch in grid.items() if ch == '^')
 
-a1 = a2 = 0
-
-visited_p1 = set()
-
-def part1():
-    pos = start
+def run(grid):
+    p = start
     d = 0
 
-    while pos in grid:
-        visited_p1.add(pos)
-        r,c = pos
-        np = r+DIRS[d][0], c + DIRS[d][1]
-        if grid.get(np) == '#':
-            d += 1
-            d %= 4
-        else:
-            pos = np
-    return len(visited_p1)
-
-def loops(block):
-    g = grid.copy()
-    g[block] = '#'
-    pos = start
-    d = 0
-    states = set()
     visited = set()
+    states = set()
 
-    while pos in g:
-        visited.add(pos)
-        if (pos, d) in states:
-            return True
-        states.add((pos, d))
+    while p in grid:
+        visited.add(p)
+        if (p, d) in states:
+            return visited, True
+        states.add((p, d))
 
-        r,c = pos
-        np = r + DIRS[d][0], c + DIRS[d][1]
-        if g.get(np) == '#':
-            d += 1
-            d %= 4
+        np = p[0] + DIRS[d][0], p[1] + DIRS[d][1]
+        if grid.get(np) == '#':
+            d = (d + 1) % 4
         else:
-            pos = np
+            p = np
 
-        # maxrow = max(r for r,c in grid)
-        # maxcol = max(c for r,c in grid)
-        # for r in range(maxrow+1):
-        #     for c in range(maxcol+1):
-        #         print('X' if (r,c) in visited else grid[r,c], end='')
-        #     print()
-        # print()
-        # time.sleep(0.1)
+    return visited, False
 
-    return False
+visited_p1, _ = run(grid)
 
-a1 = part1()
-a2 = sum([loops(p) for p in visited_p1 if p != start])
+a1 = len(visited_p1)
+a2 = sum(run(grid | {p: '#'})[1] for p in visited_p1 if p != start)
 
 print('part1:', a1)
 print('part2:', a2)
