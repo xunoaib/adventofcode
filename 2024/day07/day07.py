1f354f06046c54937616bd300ecbd5764e5502e8
xunoaib <xunoaib@gmail.com>
2024-12-07 00:11:36 -0500

    Day 7, 2024 - Part 2

--- 2024/day07/day07.py
+++ 2024/day07/day07.py
@@ -4,19 +4,21 @@ import sys
 from itertools import product
 
 
-def canmake(tot, nums):
-    allops = list(product('+*', repeat=len(nums)-1))
+def canmake(tot, nums, operators):
+    allops = list(product(operators, repeat=len(nums)-1))
 
     for ops in allops:
         a = nums[0]
         for v,op in zip(nums[1:], ops):
             if op == '+':
                 a += v
-            else:
+            elif op == '*':
                 a *= v
+            elif op == '|':
+                a = int(f'{a}{v}')
         if a == tot:
-            return True
-    return False
+            return tot
+    return 0
 
 d = {}
 
@@ -24,12 +26,11 @@ for line in sys.stdin:
     a, b = line.strip().split(':')
     d[int(a)] = list(map(int, b.split()))
 
-a = 0
-for tot, nums in d.items():
-    if canmake(tot, nums):
-        print('YES', tot, nums)
-        a += tot
-    else:
-        print('NO ', tot, nums)
+a1 = sum(canmake(tot, nums, '+*') for tot, nums in d.items())
+print('part1:', a1)
 
-print('part1:', a)
+a2 = sum(canmake(tot, nums, '+*|') for tot, nums in d.items())
+print('part2:', a2)
+
+assert a1 == 4998764814652
+assert a2 == 37598910447546
