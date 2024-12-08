becf67395f41b60cdeffcd108561ff0d85045940
xunoaib <xunoaib@gmail.com>
2024-12-08 00:28:24 -0500

    Day 8, 2024 - Part 2 Tweaks

--- 2024/day08/day08.py
+++ 2024/day08/day08.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-from collections import Counter, defaultdict
-from itertools import combinations, pairwise, permutations, product
-
-DIRS = U, D, L, R = (-1, 0), (1, 0), (0, -1), (0, 1)
-
+from collections import defaultdict
+from itertools import combinations
 
 lines = sys.stdin.read().strip().split('\n')
 
@@ -15,59 +12,40 @@ grid = {
     for c, ch in enumerate(line)
 }
 
-maxrow = max(r for r,c in grid.keys())
-maxcol = max(c for r,c in grid.keys())
-
-freqposs = defaultdict(set)
-for p, f in grid.items():
+fpositions = defaultdict(set)
+for pos, f in grid.items():
     if f != '.':
-        freqposs[f].add(p)
-freqposs  = dict(freqposs)
+        fpositions[f].add(pos)
 
-antinodes = set()
+antinodes1 = set()
+antinodes2 = set()
 
-g = grid.copy()
-
-for f, positions in freqposs.items():
-    print('positinos:', positions)
+for f, positions in fpositions.items():
     for p1,p2 in combinations(positions, r=2):
-        print(p1, p2)
 
-        r1,c1 = p1
-        r2,c2 = p2
-        rdiff = r2-r1
-        cdiff = c2-c1
+        rdiff = p2[0] - p1[0]
+        cdiff = p2[1] - p1[1]
 
-        q = r1, c1
-        while q in grid:
-            # if q not in (p1, p2):
-            antinodes.add(q)
-            q = q[0] - rdiff, q[1] - cdiff
+        antinodes1 |= {
+            (p1[0] - rdiff, p1[1] - cdiff),
+            (p2[0] + rdiff, p2[1] + cdiff)
+        }
 
-        q = r1, c1
-        while q in grid:
-            # if q not in (p1, p2):
-            antinodes.add(q)
-            q = q[0] + rdiff, q[1] + cdiff
+        p = p1
+        while p in grid:
+            antinodes2.add(p)
+            p = p[0] - rdiff, p[1] - cdiff
 
-        # p4 = r2 + rdiff, c2 + cdiff
-        # assert not {q, p4} & {p1, p2}
+        p = p1
+        while p in grid:
+            antinodes2.add(p)
+            p = p[0] + rdiff, p[1] + cdiff
 
-        # antinodes |= {q,p4}
+a1 = len(antinodes1 & set(grid))
+a2 = len(antinodes2 & set(grid))
 
-antinodes = {(r,c) for (r,c) in antinodes if r in range(maxrow+1) and c in range(maxcol+1)}
-
-for r,c in antinodes:
-    g[r,c] = '#'
-
-for r in range(maxrow+1):
-    for c in range(maxcol+1):
-        print(g[r,c], end='')
-    print()
-
-a2 = len(antinodes)
-
+print('part1:', a1)
 print('part2:', a2)
 
-# assert a1 == 0
-# assert a2 == 0
+assert a1 == 357
+assert a2 == 1266
