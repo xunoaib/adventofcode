292b7f29049b3dad2b72822ba3213ff44cc0818d
xunoaib <xunoaib@gmail.com>
2024-12-11 00:24:08 -0500

    Day 11, 2024: Recursive Part 2 is too slow

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -2,23 +2,48 @@
 
 
 import sys
+from functools import cache
 
 stones = tuple(map(int, input().split()))
 
-for i in range(25):
-    new = []
-    for s in stones:
-        if s == 0:
-            new.append(1)
-        elif len(str(s)) % 2 == 0:
-            t = str(s)
-            new += [int(t[:len(t)//2]), int(t[len(t)//2:])]
-        else:
-            new.append(s * 2024)
-    stones = new
+# for i in range(25):
+#     new = []
+#     for s in stones:
+#         if s == 0:
+#             new.append(1)
+#         elif len(str(s)) % 2 == 0:
+#             t = str(s)
+#             new += [int(t[:len(t)//2]), int(t[len(t)//2:])]
+#         else:
+#             new.append(s * 2024)
+#     stones = new
+# x = len(stones)
+# print('part1:', x)
 
-x = len(stones)
-print('part1:', x)
+@cache
+def split(num, d):
+    # print(d, num)
+    if d == 0:
+        return [num]
+
+    if num == 0:
+        return [v for v in split(1, d-1)]
+    elif len(str(num)) % 2 == 0:
+        t = str(num)
+        a,b = [int(t[:len(t)//2]), int(t[len(t)//2:])]
+        vs1 = [v for v in split(a, d-1)]
+        vs2 = [v for v in split(b, d-1)]
+        return vs1+vs2
+    else:
+        return [v for v in split(num * 2024, d-1)]
+
+r = []
+for i,n in enumerate(stones):
+    print(i,n)
+    r += split(n, 75)
+
+a2 = len(r)
+print('part2:', a2)
 
 # print('part1:', a1)
 # print('part2:', a2)
