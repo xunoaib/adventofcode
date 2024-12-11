34a87d814ec8c2e67eeffc42293e938e30a92486
xunoaib <xunoaib@gmail.com>
2024-12-11 00:51:02 -0500

    Day 11, 2024 - Part 2

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,52 +1,27 @@
 #!/usr/bin/env python3
-
-
-import sys
+from collections import Counter
 from functools import cache
 
-stones = tuple(map(int, input().split()))
-
-# for i in range(25):
-#     new = []
-#     for s in stones:
-#         if s == 0:
-#             new.append(1)
-#         elif len(str(s)) % 2 == 0:
-#             t = str(s)
-#             new += [int(t[:len(t)//2]), int(t[len(t)//2:])]
-#         else:
-#             new.append(s * 2024)
-#     stones = new
-# x = len(stones)
-# print('part1:', x)
 
 @cache
 def split(num, d):
-    # print(d, num)
     if d == 0:
-        return [num]
-
+        return 1
     if num == 0:
-        return [v for v in split(1, d-1)]
+        return split(1, d-1)
     elif len(str(num)) % 2 == 0:
-        t = str(num)
-        a,b = [int(t[:len(t)//2]), int(t[len(t)//2:])]
-        vs1 = [v for v in split(a, d-1)]
-        vs2 = [v for v in split(b, d-1)]
-        return vs1+vs2
-    else:
-        return [v for v in split(num * 2024, d-1)]
+        s = str(num)
+        left, right = [int(s[:len(s)//2]), int(s[len(s)//2:])]
+        return split(left, d-1) + split(right, d-1)
+    return split(num * 2024, d-1)
 
-r = []
-for i,n in enumerate(stones):
-    print(i,n)
-    r += split(n, 75)
+counts = Counter(tuple(map(int, input().split())))
 
-a2 = len(r)
+a1 = sum(count * split(n, 25) for n, count in counts.items())
+a2 = sum(count * split(n, 75) for n, count in counts.items())
+
+print('part1:', a1)
 print('part2:', a2)
 
-# print('part1:', a1)
-# print('part2:', a2)
-
-# assert a1 == 0
-# assert a2 == 0
+assert a1 == 203953
+assert a2 == 242090118578155
