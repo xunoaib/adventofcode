894bbac8d7111f75d187e66240f8f7b7ccc67782
xunoaib <xunoaib@gmail.com>
2024-12-12 01:45:08 -0500

    Day 12, 2024 - Part 2 Tweaks

--- 2024/day12/day12.py
+++ 2024/day12/day12.py
@@ -4,63 +4,35 @@ import sys
 from collections import defaultdict
 from itertools import pairwise
 
-DIRS = U, D, L, R = (-1, 0), (1, 0), (0, -1), (0, 1)
 
 def neighbors4(r, c):
-    for roff, coff in DIRS:
+    for roff, coff in (-1, 0), (1, 0), (0, -1), (0, 1):
         if not (roff and coff):
             yield r + roff, c + coff
 
-lines = sys.stdin.read().strip().split('\n')
-
-grid = {
-    (r, c): ch
-    for r, line in enumerate(lines)
-    for c, ch in enumerate(line)
-}
-
-def find_perim(locs):
-    tot = 0
-    for p in locs:
-        for n in neighbors4(*p):
-            if n not in locs:
-                tot += 1
-    return tot
-
-def find_pool(r,c):
-    ch = grid[r,c]
-    pool = {(r,c)}
-    seen = {(r,c)}
-    q = [(r,c)]
+def find_pool(pos):
+    ch = grid[pos]
+    pool = {pos}
+    q = [pos]
     while q:
-        p = q.pop()
-        for n in neighbors4(*p):
-            if n not in seen and grid.get(n) == ch:
-                seen.add(n)
-                q.append(n)
+        pos = q.pop()
+        for n in neighbors4(*pos):
+            if n not in pool and grid.get(n) == ch:
                 pool.add(n)
+                q.append(n)
     return pool
 
-pools = []
-pooled = set()
+def find_pools():
+    pools = []
+    pooled = set()
+    for p in grid:
+        if p not in pooled:
+            pool = find_pool(p)
+            pools.append(pool)
+            pooled |= pool
+    return pools
 
-for p, ch in grid.items():
-    if p not in pooled:
-        pool = find_pool(*p)
-        pools.append(pool)
-        pooled |= pool
-
-a1 = 0
-for pool in pools:
-    perim = find_perim(pool)
-    area = len(pool)
-    a1 += perim * area
-print('part1:', a1)
-
-def sub(a,b):
-    return b[0]-a[0], b[1]-a[1]
-
-def find_borders(pool):
+def find_border_vectors(pool):
     borders = set()
     for p in pool:
         for n in neighbors4(*p):
@@ -68,39 +40,36 @@ def find_borders(pool):
                 borders.add((p, sub(n,p)))
     return borders
 
+def sub(a,b):
+    return b[0]-a[0], b[1]-a[1]
+
+grid = {
+    (r, c): ch
+    for r, line in enumerate(sys.stdin)
+    for c, ch in enumerate(line.strip())
+}
+
+pools = find_pools()
+
+a1 = sum(len(pool) * sum(n not in pool for p in pool for n in neighbors4(*p)) for pool in pools)
 a2 = 0
+
 for pool in pools:
-    borders = find_borders(pool)
+    borders = find_border_vectors(pool)
 
-    sided = defaultdict(set)
-    for p, diff in borders:
-        sided[diff].add(p)
-
-    ch = grid[list(pool)[0]]
-
-    # if ch not in 'V':
-    #     continue
+    vec_tiles = defaultdict(set)
+    for p, vec in borders:
+        vec_tiles[vec].add(p)
 
     sides = 0
-    for diff, poss in sided.items():
-        coff, roff = diff
-        poss = sorted(poss) if coff else sorted(poss, key=lambda v: v[::-1])
+    for vec, tiles in vec_tiles.items():
+        coff, roff = vec  # flip axes to get fence direction
+        tiles = sorted(tiles, key=lambda v: v if coff else v[::-1])
+        sides += 1 + sum(tuple(map(abs, sub(b, a))) != (abs(roff), abs(coff)) for a,b in pairwise(tiles))
 
-        # print(diff, '=>', poss)
+    a2 += len(pool) * sides
 
-        sides += 1  # at least one side
-        for a,b in pairwise(poss):
-            if tuple(map(abs, sub(b, a))) == (abs(roff), abs(coff)):
-                # print('    adjacent    ', a, b, diff)
-                sides += 0  # same side
-            else:
-                sides += 1  # new side
-                # print('    not adjacent', a, b, diff)
-
-    # print(ch, len(pool), '*', sides, '=', len(pool)  * sides)
-    a2 += len(pool)  * sides
-
-
+print('part1:', a1)
 print('part2:', a2)
 
 assert a1 == 1424006
