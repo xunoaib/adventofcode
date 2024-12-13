b1af651fb04b255e35faec93684716cf22eb5b06
xunoaib <xunoaib@gmail.com>
2024-12-13 00:25:33 -0500

    Day 13, 2024 - Part 2

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -3,43 +3,42 @@ import re
 import sys
 from itertools import batched
 
-from z3 import Int, Ints, Optimize, Solver, sat
+from z3 import Ints, Optimize, sat
 
 lines = [line for line in sys.stdin.read().splitlines() if line.strip()]
 
-a1 = a2 = 0
+def solve(prize_offset):
+    tot = 0
+    for a,b,c in batched(lines, 3):
+        a_x = a_y = b_x = b_y = p_x = p_y = 0
+        if m := re.match(r'.*: X+(.*), Y+(.*)', a):
+            a_x, a_y = map(int, m.groups())
+        if m := re.match(r'.*: X+(.*), Y+(.*)', b):
+            b_x, b_y = map(int, m.groups())
+        if m := re.match(r'.*: X=(.*), Y=(.*)', c):
+            p_x, p_y = map(int, m.groups())
+            p_x += prize_offset
+            p_y += prize_offset
 
-for a,b,c in batched(lines, 3):
-    a_x = a_y = b_x = b_y = p_x = p_y = 0
-    if m := re.match(r'.*: X+(.*), Y+(.*)', a):
-        a_x, a_y = map(int, m.groups())
-    if m := re.match(r'.*: X+(.*), Y+(.*)', b):
-        b_x, b_y = map(int, m.groups())
-    if m := re.match(r'.*: X=(.*), Y=(.*)', c):
-        p_x, p_y = map(int, m.groups())
+        zac, zac, zbc, zbc, tokens = Ints('zaxc zayc zbxc zbyc tokens')
 
-    zax, zay, zbx, zby, zpx, zpy = Ints('zax zay zbx zby zpx zpy')
-    zac, zac, zbc, zbc, = Ints('zaxc zayc zbxc zbyc')
-    tokens = Int('tokens')
+        s = Optimize()
+        s.add(p_x == zac * a_x + zbc * b_x)
+        s.add(p_y == zac * a_y + zbc * b_y)
+        s.add(zac >= 0)
+        s.add(zbc >= 0)
+        s.add(tokens == 3 * zac + 1 * zbc)
+        s.minimize(tokens)
 
-    s = Optimize()
-    s.add(p_x == zac * a_x + zbc * b_x)
-    s.add(p_y == zac * a_y + zbc * b_y)
-    s.add(zac >= 0)
-    s.add(zbc >= 0)
-    s.add(tokens == (3 * zac + 1 * zbc))
-    s.minimize(tokens)
+        if s.check() == sat:
+            tot += s.model()[tokens].as_long()
+    return tot
 
-    if s.check() == sat:
-        m = s.model()
-        vals = [m[x].as_long() for x in [zac, zbc]]
-        print(vals)
-        print(m[tokens].as_long())
-        a1 += m[tokens].as_long()
-
+a1 = solve(0)
+a2 = solve(10000000000000)
 
 print('part1:', a1)
-# print('part2:', a2)
+print('part2:', a2)
 
-# assert a1 == 0
-# assert a2 == 0
+assert a1 == 26810
+assert a2 == 108713182988244
