65f08ec7080cf66770264e11cb6c1d920e7fe37b
xunoaib <xunoaib@gmail.com>
2024-12-15 04:18:43 -0500

    Day 15, 2024 - Simplify

--- 2024/day15/day15.py
+++ 2024/day15/day15.py
@@ -13,32 +13,16 @@ data,moves = sys.stdin.read().strip().split('\n\n')
 lines = data.split('\n')
 moves = moves.replace('\n', '')
 
-def part1():
-
-    def print_grid():
-        maxr = max(r for r,c in walls)
-        maxc = max(c for r,c in walls)
-        for r in range(maxr+1):
-            for c in range(maxc+1):
-                p = r,c
-                if p == pos:
-                    print('@', end='')
-                elif p in walls:
-                    print('#', end='')
-                elif p in boxes:
-                    print('O', end='')
-                else:
-                    print('.', end='')
-            print()
-        print()
-
-    grid = {
+def to_grid(lines):
+    return {
         (r, c): ch
         for r, line in enumerate(lines)
         for c, ch in enumerate(line)
         if ch != '.'
     }
 
+def part1():
+    grid = to_grid(lines)
     boxes = {p for p, ch in grid.items() if ch == 'O'}
     walls = {p for p, ch in grid.items() if ch == '#'}
     pos = next(p for p, ch in grid.items() if ch == '@')
@@ -73,47 +57,18 @@ def part1():
     return sum(100 * r + c for r,c in boxes)
 
 def part2():
-
-    def print_grid(highlight=None):
-        maxr = max(r for r,c in walls)
-        maxc = max(c for r,c in walls)
-        for r in range(maxr+1):
-            for c in range(maxc+1):
-                p = r,c
-                ch = ident(p)
-                if p == pos:
-                    ch = f'\033[91m{ch}\033[0m'
-                elif highlight and p in highlight:
-                    ch = f'\033[93m{ch}\033[0m'
-                print(ch, end='')
-            print()
-        print()
-
     d = data
     d = d.replace('#','##')
     d = d.replace('O','[]')
     d = d.replace('.','..')
     d = d.replace('@','@.')
     lines = d.split('\n')
-
-    grid = {
-        (r, c): ch
-        for r, line in enumerate(lines)
-        for c, ch in enumerate(line)
-        if ch != '.'
-    }
+    grid = to_grid(lines)
 
     lboxes = {p for p, ch in grid.items() if ch == '['}
     rboxes = {p for p, ch in grid.items() if ch == ']'}
     walls = {p for p, ch in grid.items() if ch == '#'}
     pos = next(p for p, ch in grid.items() if ch == '@')
-
-    def nextboxpos(cur, direction):
-        while cur in boxes:
-            cur = cur[0]+direction[0], cur[1]+direction[1]
-        if cur in walls:
-            return False
-        return cur
 
     def horiz_pushable(cur, direction):
         boxes = set()
@@ -124,20 +79,12 @@ def part2():
             return set()
         return boxes
 
-    def ident(p):
-        if p == pos:
-            return '@'
-        elif p in walls:
-            return '#'
-        elif p in lboxes:
-            return '['
-        elif p in rboxes:
-            return ']'
-        else:
-            return '.'
-
     def vert_pushable(cur, direction):
-        '''Performs the initial push check (and initiates the next two if needed)'''
+        def inner(cur):
+            npos = cur[0]+direction[0], cur[1]+direction[1]
+            if spots := vert_pushable(npos, direction):
+                return spots | {cur}
+            return None
 
         if cur in walls:
             return None
@@ -150,46 +97,16 @@ def part2():
         else:
             return {cur}
 
-        lspots = vert_pushable_inner(lpos, direction)
-        rspots = vert_pushable_inner(rpos, direction)
+        lspots = inner(lpos)
+        rspots = inner(rpos)
 
         if lspots and rspots:
             return {lpos, rpos} | lspots | rspots
 
-        return None
-
-    def vert_pushable_inner(cur, direction):
-
-        if cur in walls:
-            return None
-
-        npos = cur[0]+direction[0], cur[1]+direction[1]
-
-        if npos in walls:
-            return None  # cant push
-
-        # pushing into another block
-        if npos in rboxes:
-            r_pos = npos[0], npos[1]
-            l_pos = npos[0], npos[1]-1
-        elif npos in lboxes:
-            r_pos = npos[0], npos[1]+1
-            l_pos = npos[0], npos[1]
-        else:
-            return {cur}  # can push current block into empty space
-
-        lspots = vert_pushable_inner(l_pos, direction)
-        rspots = vert_pushable_inner(r_pos, direction)
-
-        if lspots and rspots:
-            return lspots | rspots | {cur}
-
-        return None
-
     def apply(move):
         nonlocal pos, lboxes, rboxes
-        direction = roff, coff = dirs[move]
-        npos = pos[0]+roff, pos[1]+coff
+        direction = dirs[move]
+        npos = pos[0]+direction[0], pos[1]+direction[1]
         if npos in walls:
             return
 
