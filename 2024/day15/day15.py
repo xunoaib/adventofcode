884b78ffebf7f678e87f796b56a6fc4d222dec17
xunoaib <xunoaib@gmail.com>
2024-12-15 00:38:01 -0500

    print grid 2

--- 2024/day15/day15.py
+++ 2024/day15/day15.py
@@ -13,7 +13,9 @@ data,moves = sys.stdin.read().strip().split('\n\n')
 lines = data.split('\n')
 moves = moves.replace('\n', '')
 
+
 def part1():
+
     def print_grid():
         maxr = max(r for r,c in walls)
         maxc = max(c for r,c in walls)
@@ -76,9 +78,85 @@ def part1():
         a1 += 100 * r + c
     return a1
 
+def part2():
+
+    def print_grid():
+        maxr = max(r for r,c in walls)
+        maxc = max(c for r,c in walls)
+        for r in range(maxr+1):
+            for c in range(maxc+1):
+                p = r,c
+                if p == pos:
+                    print('@', end='')
+                elif p in walls:
+                    print('#', end='')
+                elif p in lboxes:
+                    print('[', end='')
+                elif p in rboxes:
+                    print(']', end='')
+                else:
+                    print('.', end='')
+            print()
+        print()
+
+    def nextboxpos(cur, direction):
+        while cur in boxes:
+            cur = cur[0]+direction[0], cur[1]+direction[1]
+        if cur in walls:
+            return False
+        return cur
+
+    def apply(move):
+        nonlocal pos
+        r, c = pos
+        direction = roff, coff = dirs[move]
+        npos = r+roff, c+coff
+        if npos in walls:
+            return
+        if npos in boxes:
+            # if box can be pushed
+            if npos2 := nextboxpos(npos, direction):
+                if npos2 in walls | boxes:
+                    return
+                boxes.remove(npos)
+                boxes.add(npos2)
+                pos = npos
+            else:
+                return
+        pos = npos
+
+    d = data
+    d = d.replace('#','##')
+    d = d.replace('O','[]')
+    d = d.replace('.','..')
+    d = d.replace('@','@.')
+    lines = d.split('\n')
+
+    grid = {
+        (r, c): ch
+        for r, line in enumerate(lines)
+        for c, ch in enumerate(line)
+        if ch != '.'
+    }
+
+    lboxes = {p for p, ch in grid.items() if ch == '['}
+    rboxes = {p for p, ch in grid.items() if ch == ']'}
+    walls = {p for p, ch in grid.items() if ch == '#'}
+    pos = next(p for p, ch in grid.items() if ch == '@')
+
+    for i,m in enumerate(moves):
+        apply(m)
+
+    a1 = 0
+    for r,c in boxes:
+        a1 += 100 * r + c
+    return a1
+
 a1 = part1()
+a2 = part2()
+
 print('part1:', a1)
-# print('part2:', a2)
+print('part2:', a2)
 
 assert a1 == 1436690
 # assert a2 == 0
