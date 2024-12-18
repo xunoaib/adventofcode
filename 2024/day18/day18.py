8da421b34c8ee316324279a69a552646d8c4092e
xunoaib <xunoaib@gmail.com>
2024-12-18 03:19:49 -0500

    Day 18, 2024 - Tweaks

--- 2024/day18/day18.py
+++ 2024/day18/day18.py
@@ -1,60 +1,45 @@
 #!/usr/bin/env python3
 
 import sys
-from heapq import heappop, heappush
-from time import time
-
-DIRS = U, R, D, L = (-1, 0), (0, 1), (1, 0), (0, -1)
 
 
 def neighbors4(r, c):
-    for roff, coff in DIRS:
+    for roff, coff in (-1, 0), (0, 1), (1, 0), (0, -1):
         if not (roff and coff):
             yield r + roff, c + coff
 
-def shortest(corrupted):
-    start = (0,0)
-    goal = (WIDTH-1, HEIGHT-1)
+def shortest(bites):
+    bites = set(bites)
+    q = [(0, (0,0))]
+    seen = {(0,0)}
 
-    q = [(0, start)]
-    seen = {start}
     while q:
-        step, pos = heappop(q)
-        if pos == goal:
+        step, pos = q.pop(0)
+        if pos == (WIDTH-1, HEIGHT-1):
             return step
 
         for r,c in neighbors4(*pos):
             if all([
                 r in range(HEIGHT),
                 c in range(WIDTH),
-                (r,c) not in corrupted,
+                (r,c) not in bites,
                 (r,c) not in seen
             ]):
-                heappush(q, (step + 1, (r,c)))
+                q.append((step + 1, (r,c)))
                 seen.add((r,c))
 
 WIDTH = HEIGHT = 71
 
 lines = sys.stdin.read().strip().split('\n')
-all_bites = [tuple(map(int, line.split(',')))[::-1] for line in lines]
-
-def part1():
-    bites = set(all_bites[:1024])
-    return shortest(bites)
+bites = [tuple(map(int, line.split(',')))[::-1] for line in lines]
 
 def part2():
-    last = time()
-    for i in range(len(all_bites)):
-        bites = set(all_bites[:i])
-        if not shortest(bites):
-            r,c = all_bites[i-1]
+    for i in range(len(bites)):
+        if not shortest(bites[:i+1]):
+            r,c = bites[i]
             return f'{c},{r}'
 
-        if time() - last > 1:
-            print('checked step', i)
-            last = time()
-
-a1 = part1()
+a1 = shortest(bites[:1024])
 print('part1:', a1)
 
 a2 = part2()
