4e8ecf8a9061b1eff29a9302fa63c392ee297bea
xunoaib <xunoaib@gmail.com>
2024-12-24 02:14:39 -0500

    Return gate objects instead of names

--- 2024/day24/day24.py
+++ 2024/day24/day24.py
@@ -4,7 +4,7 @@ import sys
 from collections import Counter, defaultdict
 from dataclasses import dataclass
 from heapq import heappop, heappush
-from itertools import pairwise, permutations, product
+from itertools import combinations, pairwise, permutations, product
 
 from z3 import Bool, Solver, sat
 
@@ -57,12 +57,21 @@ def part1():
         vals  = [bool(m[z]) for z in zs][::-1]
         return int(''.join(['1' if v else '0' for v in vals]), 2)
 
-@dataclass
+@dataclass(order=True, frozen=True)
 class Gate:
+    out: str
     op: str
     in1: str
     in2: str
-    out: str
+
+    def __repr__(self):
+        v1 = wires[self.in1]
+        v2 = wires[self.in2]
+        name1 = v1.out if isinstance(v1, Gate) else v1
+        name2 = v2.out if isinstance(v2, Gate) else v2
+        # return f'{self.out} <== {name1} {self.op} {name2}'
+        # return f'Gate({self.out})'
+        return f'{self.out}'
 
     def evaluate(self, wires):
         v1 = wires[self.in1]
@@ -84,6 +93,7 @@ class Gate:
         raise NotImplementedError('arst')
 
 def part2():
+    global wires
 
     def print_trace(name, indent=0):
         var = wires[name]
@@ -109,7 +119,7 @@ def part2():
 
     for line in b:
         vin1, op, vin2, _, vout = line.split()
-        wires[vout] = gate = Gate(op, vin1, vin2, vout)
+        wires[vout] = gate = Gate(vout, op, vin1, vin2)
 
     x_vs = [v for w,v in sorted(wires.items()) if w.startswith('x')][::-1]
     x_bstr = ''.join('1' if v else '0' for v in x_vs)
@@ -120,38 +130,37 @@ def part2():
     y_value = int(y_bstr, 2)
 
     z_value = x_value + y_value  # REAL operation for part 2
-    z_value = x_value & y_value  # SAMPLE operation
+    # z_value = x_value & y_value  # SAMPLE operation
 
     z_vs = sorted([w for w in wires if w.startswith('z')])[::-1]
     z_bstr = ''.join('1' if wires[z].evaluate(wires) else '0' for z in z_vs)
     z_calc = int(z_bstr, 2)
 
     def find_invalid():
+        ''' Find all invalid z bits and the gates/values involved with them '''
+        bad = []
         for i, z in enumerate(z_vs[::-1]):
             g = wires[z]
             actual = g.evaluate(wires)
             expected = (z_value >> i) & 1
-            color = '\033[92m' if actual == expected else '\033[91m'
-            print(f'{color}{z} {expected} {actual} {g}\033[0m')
+            # color = '\033[92m' if actual == expected else '\033[91m'
+            # print(f'{color}{z} {expected} {actual} {g}\033[0m')
+            if actual != expected:
+                involved = {wires[v] for v in trace(z) if v[0] not in 'xy'}
+                bad.append(sorted(involved)[::-1])
+        return sorted(bad, key=len)
 
-    print(f'{x_value:>7b}')
-    print(f'{y_value:>7b}')
-    print('-'*7)
-    print(f'{z_value:>7b} expected')
-    print(f'{z_calc:>7b} actual')
-    print()
-    print(f'{x_value} + {y_value} = {z_value} (got {z_calc})')
-    print()
+    # print(f'{x_value:>7b}')
+    # print(f'{y_value:>7b}')
+    # print('-'*7)
+    # print(f'{z_value:>7b} expected')
+    # print(f'{z_calc:>7b} actual')
+    # print()
+    # print(f'{x_value} + {y_value} = {z_value} (got {z_calc})')
+    # print()
 
-    find_invalid()
-    exit(0)
-
-    for n, v in sorted(wires.items()):
-        if not n.startswith('z'):
-            continue
-        if n[0] in 'xy':
-            continue
-        print(n, trace(n))
+    bad = find_invalid()
+    __import__('pprint').pprint(bad)
 
 # a1 = part1()
 # print('part1:', a1)
