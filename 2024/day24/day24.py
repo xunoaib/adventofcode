df147d5239d2aa49af8f723487d62dfa4b38dc0b
xunoaib <xunoaib@gmail.com>
2024-12-27 20:06:20 -0500

    Day 24 Part 2 WIP

--- 2024/day24/day24.py
+++ 2024/day24/day24.py
@@ -8,6 +8,9 @@ from itertools import combinations, pairwise, permutations, product
 
 from z3 import Bool, Solver, sat
 
+
+class LoopError(Exception): ...
+
 # @dataclass
 # class Gate:
 #     gtype: str
@@ -57,31 +60,40 @@ def part1():
         vals  = [bool(m[z]) for z in zs][::-1]
         return int(''.join(['1' if v else '0' for v in vals]), 2)
 
-@dataclass(order=True, frozen=True)
+@dataclass(order=True)
 class Gate:
     out: str
     op: str
     in1: str
     in2: str
 
-    def __repr__(self):
-        v1 = wires[self.in1]
-        v2 = wires[self.in2]
-        name1 = v1.out if isinstance(v1, Gate) else v1
-        name2 = v2.out if isinstance(v2, Gate) else v2
-        # return f'{self.out} <== {name1} {self.op} {name2}'
-        # return f'Gate({self.out})'
-        return f'{self.out}'
+    def __hash__(self):
+        return hash((self.out, self.op, self.in1, self.in2))
 
-    def evaluate(self, wires):
+    # def __repr__(self):
+    #     v1 = wires[self.in1]
+    #     v2 = wires[self.in2]
+    #     name1 = v1.out if isinstance(v1, Gate) else v1
+    #     name2 = v2.out if isinstance(v2, Gate) else v2
+    #     # return f'{self.out} <== {name1} {self.op} {name2}'
+    #     # return f'Gate({self.out})'
+    #     return f'{self.out}'
+
+    def evaluate(self, wires, seen=tuple()):
+
+        if self in seen:
+            raise LoopError()
+
+        seen += (self,)
+
         v1 = wires[self.in1]
         v2 = wires[self.in2]
 
         if isinstance(v1, Gate):
-            v1 = v1.evaluate(wires)
+            v1 = v1.evaluate(wires, seen)
 
         if isinstance(v2, Gate):
-            v2 = v2.evaluate(wires)
+            v2 = v2.evaluate(wires, seen)
 
         match self.op:
             case 'XOR':
@@ -119,7 +131,7 @@ def part2():
 
     for line in b:
         vin1, op, vin2, _, vout = line.split()
-        wires[vout] = gate = Gate(vout, op, vin1, vin2)
+        wires[vout] = Gate(vout, op, vin1, vin2)
 
     x_vs = [v for w,v in sorted(wires.items()) if w.startswith('x')][::-1]
     x_bstr = ''.join('1' if v else '0' for v in x_vs)
@@ -129,12 +141,16 @@ def part2():
     y_bstr = ''.join('1' if v else '0' for v in y_vs)
     y_value = int(y_bstr, 2)
 
-    z_value = x_value + y_value  # REAL operation for part 2
-    # z_value = x_value & y_value  # SAMPLE operation
+    # import random
+    # x_value = random.randint(1,(1 << len(x_vs))-1)
+    # y_value = random.randint(1,(1 << len(y_vs))-1)
+
+    z_expected = x_value + y_value  # REAL operation for part 2
+    # z_expected = x_value & y_value  # SAMPLE operation
 
     z_vs = sorted([w for w in wires if w.startswith('z')])[::-1]
     z_bstr = ''.join('1' if wires[z].evaluate(wires) else '0' for z in z_vs)
-    z_calc = int(z_bstr, 2)
+    z_actual = int(z_bstr, 2)
 
     def find_invalid():
         ''' Find all invalid z bits and the gates/values involved with them '''
@@ -142,13 +158,55 @@ def part2():
         for i, z in enumerate(z_vs[::-1]):
             g = wires[z]
             actual = g.evaluate(wires)
-            expected = (z_value >> i) & 1
+            expected = (z_expected >> i) & 1
             # color = '\033[92m' if actual == expected else '\033[91m'
             # print(f'{color}{z} {expected} {actual} {g}\033[0m')
             if actual != expected:
-                involved = {wires[v] for v in trace(z) if v[0] not in 'xy'}
+                # involved = {wires[v] for v in trace(z) if v[0] not in 'xy'}
+                involved = {v for v in trace(z) if v[0] not in 'xy'}
                 bad.append(sorted(involved)[::-1])
         return sorted(bad, key=len)
+
+    gs = find_invalid()
+    for i,g in enumerate(gs):
+        print(i,g)
+
+    print(gs)
+    # for x,y in combinations(gs, r=2):
+    #     if set(x) & set(y):
+    #         print(x,y)
+    exit(0)
+
+    used = set()
+
+    def dfs(bad, left=4, seq=tuple()):
+        nonlocal used
+
+        print(seq)
+
+        if not bad:
+            print(wires)
+            return True
+
+        if left == 0:
+            return not bad
+
+        gates = sorted({gate for group in bad for gate in group} - used)
+        for a, b in combinations(gates, r=2):
+
+            wires[a], wires[b] = wires[b], wires[a]
+            used |= {a,b}
+
+            try:
+                newbad = find_invalid()
+                if dfs(newbad, left-1, seq + (a,b)):
+                    return True
+                # print(newbad)
+            except LoopError:
+                pass
+
+            wires[a], wires[b] = wires[b], wires[a]
+            used -= {a,b}
 
     # print(f'{x_value:>7b}')
     # print(f'{y_value:>7b}')
@@ -161,6 +219,12 @@ def part2():
 
     bad = find_invalid()
     __import__('pprint').pprint(bad)
+
+    try:
+        success = dfs(bad)
+        print(success)
+    except RecursionError as exc:
+        print('recursion error', exc)
 
 # a1 = part1()
 # print('part1:', a1)
