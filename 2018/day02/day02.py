72da647bf6736aadd0882880ce40f22d86ae9205
xunoaib <xunoaib@gmail.com>
2025-05-13 03:12:33 -0400

    2018 - Day 2, Part 2

--- 2018/day02/day02.py
+++ 2018/day02/day02.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 import sys
 from collections import Counter
+from itertools import combinations
 
 lines = sys.stdin.read().strip().split('\n')
 
-count2 = count3 = 0
 
-for line in lines:
-    freq = Counter(line)
-    count2 += 2 in freq.values()
-    count3 += 3 in freq.values()
+def part1():
+    count2 = count3 = 0
+    for line in lines:
+        freq = Counter(line)
+        count2 += 2 in freq.values()
+        count3 += 3 in freq.values()
+    return count2 * count3
 
-a1 = count2 * count3
 
-print('part1:', a1)
+def part2():
+    for a, b in combinations(lines, r=2):
+        if sum(x != y for x, y in zip(a, b)) == 1:
+            return ''.join(x for x, y in zip(a, b) if x == y)
 
-assert a1 == 8820
+
+p1 = part1()
+p2 = part2()
+
+print('part1:', p1)
+print('part2:', p2)
+
+assert p1 == 8820
+assert p2 == 'bpacnmglhizqygfsjixtkwudr'
