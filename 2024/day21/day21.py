2a21e5ff56a40b0f85f79957e6df7c5216d8ebe7
xunoaib <xunoaib@gmail.com>
2025-05-14 03:43:55 -0400

    Clean up

--- 2024/day21/day21.py
+++ 2024/day21/day21.py
@@ -6,60 +6,47 @@ from itertools import pairwise
 
 def find_path(v1: str, v2: str):
     numpad = is_numpad(v1) or is_numpad(v2)
-    grid_pos = ngrid_pos if numpad else dgrid_pos
-    corner_pos = (3, 0) if numpad else (0, 0)
-    r1, c1 = grid_pos[v1]
-    r2, c2 = grid_pos[v2]
+    grid, corner = (NUM_GRID, (3, 0)) if numpad else (DIR_GRID, (0, 0))
+    r1, c1 = grid[v1]
+    r2, c2 = grid[v2]
 
     hchar = '>' if c2 > c1 else '<'
     vchar = 'v' if r2 > r1 else '^'
 
     path = hchar * abs(c2 - c1) + vchar * abs(r2 - r1)
-    path = ''.join(sorted(path, key=lambda c: '>v^<'.index(c)))
+    path = ''.join(sorted(path, key='>v^<'.index))
 
-    if corner_pos not in [(r2, c1), (r1, c2)]:
-        path = path[::-1]
-
-    return path
+    return path[::-1] if corner not in [(r2, c1), (r1, c2)] else path
 
 
 @cache
-def path_length(seq: str, depth: int):
+def path_length(path: str, depth: int):
     if depth < 0:
-        return len(seq) + 1
+        return len(path) + 1
 
-    t = 0
-    for v1, v2 in pairwise(f'A{seq}A'):
-        t += path_length(find_path(v1, v2), depth - 1)
-    return t
+    return sum(
+        path_length(find_path(a, b), depth - 1)
+        for a, b in pairwise(f'A{path}A'))
+
+
+def solve(length: int):
+    return sum(path_length(code[:3], length) * int(code[:3]) for code in codes)
 
 
 def is_numpad(char: str):
     return char in '0123456789'
 
 
-numeric_chars = '789\n456\n123\n.0A'
-directional_chars = '.^A\n<v>'
-
-ngrid = numeric_grid = {
-    (r, c): ch
-    for r, line in enumerate(numeric_chars.split('\n'))
-    for c, ch in enumerate(line) if ch != '.'
-}
-
-dgrid = directional_grid = {
-    (r, c): ch
-    for r, line in enumerate(directional_chars.split('\n'))
-    for c, ch in enumerate(line) if ch != '.'
-}
-
-ngrid_pos = {ch: pos for pos, ch in ngrid.items()}
-dgrid_pos = {ch: pos for pos, ch in dgrid.items()}
+def to_charpos_dict(chars: str):
+    return {
+        ch: (r, c)
+        for r, line in enumerate(chars.split('\n'))
+        for c, ch in enumerate(line) if ch != '.'
+    }
 
 
-def solve(length: int):
-    return sum(path_length(code[:3], length) * int(code[:3]) for code in codes)
-
+NUM_GRID = to_charpos_dict('789\n456\n123\n.0A')
+DIR_GRID = to_charpos_dict('.^A\n<v>')
 
 codes = sys.stdin.read().strip().split('\n')
 
