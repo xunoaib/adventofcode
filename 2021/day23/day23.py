8c10a0b5ebf017256634117e4fc38150f19686fa
xunoaib <xunoaib@gmail.com>
2025-05-14 00:24:28 -0400

    Remove unused code

--- 2021/day23/day23.py
+++ 2021/day23/day23.py
@@ -2,7 +2,6 @@
 
 # NOTE: This code is oriented towards Part 2, and solves it very slowly
 
-import random
 import re
 import sys
 from collections import defaultdict
@@ -165,10 +164,6 @@ class Game:
             if self.board[tar] != '.':
                 continue
 
-            # # prevent moving from hallway to hallway
-            # if 0 <= src <= 6 and 0 <= tar <= 6:
-            #     continue
-
             # enforce rules when moving in room
             if tar >= 7:
                 correct_room = (tar - 7) % 4 == 'ABCD'.index(pod)
@@ -225,7 +220,6 @@ class Game:
                 pods[ch].add(pos)
 
         # ignore letters already in the correct room
-        # unsolved = defaultdict(set)
         total_cost = 0
         for ch in 'ABCD':
             overlap = targets[ch] & pods[ch]
@@ -260,30 +254,13 @@ def next_states(state):
 
 
 def bfs(state):
-    stop_idx = random.randint(1, 13_000)
-    stop_idx = 3475
-    print(f'Stopping at iteration {stop_idx}')
-
     game = Game(state)
     q = [(game.cost_to_solve(), 0, state)]
     visited = {state: 0}
     last = time()
-    best = (sys.maxsize, state)
-    i = 0
     while q:
         f_current, neg_g, state = heappop(q)
         g = -neg_g
-
-        if i == stop_idx:
-            game = Game(state)
-            print(game.visualize())
-            print()
-            for m in game.get_all_moves():
-                src, tar, cost = m
-                s = game.signature()
-                print(s[src], s[tar], str(cost).rjust(4), (src, tar))
-            print()
-        i += 1
 
         if state.endswith('ABCD' * 4):
             print(state, g, 'solved', file=sys.stderr)
@@ -310,17 +287,6 @@ def bfs(state):
             last = time()
 
     print(f'no solution after {len(visited)} states')
-    # print(frontier, best)
-
-
-# def state_tup_fromstring(s):
-#     return (s[:7],) + tuple(s[i+7::ROOM_SIZE] for i in range(4))
-#
-# def solved_tup(state):
-#     return all(state[i+1].count(ch) == ROOM_SIZE for i, ch in enumerate('ABCD'))
-
-# def state_fromstring(s):
-#     return {i: ch for i, ch in enumerate(s) if ch in 'ABCD'}
 
 
 def state_fromstring(s):
@@ -345,76 +311,8 @@ def correct_room(pod, pos):
     return 'ABCD'.index(pod) == get_room(pos)
 
 
-# def has_strangers(state, pos):
-#     roomstart = get_room(pos) + 7
-#     return bool(set(state[roomstart:roomstart + 4]) - {'.', 'ABCD'[room]})
-
-
 def is_room(pos):
     return pos > 6
-
-
-# def next_states_pos(state, pos):
-#     pod = state[pos]
-#
-#     if pos < 7:  # in hallway
-#         pass
-#     elif correct_room(pod, pos):  # in correct room
-#         if not has_strangers(state,
-#                              pos):  # in correct romom, no further moves needed
-#             return []
-#     else:  # in incorrect room
-#         pass
-
-# def accessible(state, pos):
-#     """Find all positions accessible from the current one"""
-#     pod = state[pos]
-#     assert pod != '.'
-#
-#     targets = []
-#     visited = set()
-#     frontier = [(0, pos)]
-#     while frontier:
-#         cost, pos = heappop(frontier)
-#         visited.add(pos)
-#
-#         for neighbor, addcost in links[pos].items():
-#             if neighbor in visited:
-#                 continue
-#
-#             # prevent moving within hallway
-#             if 0 <= pos <= 6 and 0 <= neighbor <= 6:
-#                 continue
-#
-#             # enforce blocking rules
-#             if state[neighbor] != '.':
-#                 continue
-#
-#             entry = (cost + addcost * move_costs[pod], neighbor)
-#
-#             # moving into room
-#             if is_room(neighbor):
-#                 # skip adding suboptimal moves (like moving 'B' @ 10 to 7-9)
-#                 # only allow moving into hallway
-#                 # todo: move all the way into target room
-#                 if correct_room(pod, pos):
-#                     targets.append(entry)
-#                 # else: incorrect target room. skip adding a target, but add to
-#                 # frontier in case a hallway target is open
-#
-#             # moving into hallway
-#             else:
-#                 targets.append(entry)
-#
-#             heappush(frontier, entry)
-#             visited.add(neighbor)
-#     return targets
-
-# def room_to_hallway(state):
-#     """Find moves from each room to the hallway"""
-#     for r in range(4):
-#         if tup := next((i, ch for ch in enumerate(s[7+r*ROOM_SIZE:]) if ch != '.'), None):
-#             pass
 
 
 def main():
@@ -423,14 +321,6 @@ def main():
 
     data = sys.stdin.read()
     sig = re.findall(r'[A-Z\.]', data)
-
-    # solved_sig = '.......ABCDABCDABCDABCD'
-
-    # g = Game(solved_sig)
-    # print(g.visualize())
-    # print(list(g.get_moves()))
-    # print(g.cost_to_solve())
-    # return
 
     # remove hallway tiles immediately outside rooms
     for i in [8, 6, 4, 2]:
@@ -446,7 +336,6 @@ def main():
         print(s[src], s[tar], str(cost).rjust(4), (src, tar))
 
     bfs(g.signature())
-    return
 
     # assert ans1 == 15237
     # assert ans2 == 47509
