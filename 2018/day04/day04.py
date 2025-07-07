65dce157ed4344cbdbae91fecd66ef3c132d970b
xunoaib <xunoaib@gmail.com>
2025-07-07 17:20:50 -0400

    Day 4, 2018 - Part 1

---
+++ 2018/day04/day04.py
@@ -0,0 +1,34 @@
+import re
+import sys
+from collections import Counter, defaultdict
+from datetime import datetime, timedelta
+
+lines = sorted(sys.stdin.read().strip().splitlines())
+
+guard = None
+asleep_since = {}
+time_asleep = Counter()
+when_asleep = defaultdict(Counter)
+
+for line in lines:
+    datestring, message = re.match(r'\[(.*)\] (.*)', line).groups()
+    dt = datetime(*map(int, re.split(r'[-: ]', datestring)))
+
+    if 'begins shift' in message:
+        guard = int(message.split(' ')[1][1:])
+    elif message == 'falls asleep':
+        asleep_since[guard] = dt
+    elif message == 'wakes up':
+        mins = (dt - asleep_since[guard]).total_seconds() / 60
+        time_asleep[guard] += int(mins)
+        start = asleep_since[guard]
+        while start < dt:
+            when_asleep[guard][start.minute] += 1
+            start += timedelta(minutes=1)
+
+guard = time_asleep.most_common()[0][0]
+minute = when_asleep[guard].most_common()[0][0]
+a1 = guard * minute
+print('part1:', a1)
+
+assert a1 == 19830
