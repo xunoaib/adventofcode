c78f7cf8f7a7e9ccdd3003b4929a403710eade76
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-02 17:05:19 -0400

    Solve Day 12 Part 1, 2019

--- 2019/day12/day12.py
+++ 2019/day12/day12.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+import copy
 import re
 import sys
 from itertools import batched, combinations
 
 
 def apply_gravity(moons: list, velocities: list):
+    velocities = copy.deepcopy(velocities)
     delta_velocities = [[0, 0, 0] for _ in velocities]
     for m1, m2 in combinations(range(len(moons)), 2):
         p1 = moons[m1]
@@ -31,15 +33,32 @@ def apply_gravity(moons: list, velocities: list):
     ], final_velocities
 
 
-moons = list(batched(map(int, re.findall(r'-?\d+', sys.stdin.read())), 3))
-velocities = [[0, 0, 0] for i in range(len(moons))]
+def calculate_energy(moons: list, velocities: list):
+    return sum(
+        abs(sum(map(abs, pos)) * sum(map(abs, vel)))
+        for pos, vel in zip(moons, velocities)
+    )
 
-for i in range(11):
-    print(f'\nAfter {i} steps:')
 
+def print_status():
     for (x, y, z), (vx, vy, vz) in zip(moons, velocities):
         print(
             f'pos=<x={x:2}, y={y:3}, z={z:2}>, vel=<x={vx:2}, y={vy:2}, z={vz:2}>'
         )
 
+
+moons = list(batched(map(int, re.findall(r'-?\d+', sys.stdin.read())), 3))
+velocities = [(0, 0, 0) for i in range(len(moons))]
+
+print(f'\nAfter 0 steps:')
+print_status()
+
+for i in range(1000):
     moons, velocities = apply_gravity(moons, velocities)
+    print(f'\nAfter {i+1} steps:')
+    print_status()
+
+ans1 = calculate_energy(moons, velocities)
+print('part1:', ans1)
+
+assert ans1 == 9493
