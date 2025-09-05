0fb1af213cd0b5237b4b2bcd97167fd995da661e
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-05 17:45:48 -0400

    WIP add broken constraints

--- 2019/day14/day14.py
+++ 2019/day14/day14.py
@@ -3,7 +3,7 @@ from collections import Counter, defaultdict
 from dataclasses import dataclass
 from typing import Any
 
-from z3 import Int, Solver
+from z3 import Int, Optimize, Solver
 
 
 @dataclass(frozen=True)
@@ -30,13 +30,11 @@ for line in sys.stdin:
         counts.append((s, int(n)))
 
     *inputs, output = [Resource(*c) for c in counts]
-    recipe = Recipe(inputs, output)
-    recipe_lookup[output.name] = recipe
+    recipe_lookup[output.name] = recipe = Recipe(inputs, output)
     recipes.append(recipe)
 
 # Allow creating one ORE at any time
-recipe = Recipe([], Resource('ORE', 1))
-recipe_lookup['ORE'] = recipe
+recipe_lookup['ORE'] = recipe = Recipe([], Resource('ORE', 1))
 recipes.append(recipe)
 
 
@@ -80,6 +78,8 @@ class ZReaction:
         return rcounts[self.name]
 
 
+s = Optimize()
+
 # Create variables representing the total amount of each resource
 z_res = {r.output.name: Int(f'{r.output.name}') for r in recipes}
 
@@ -88,24 +88,41 @@ rcounts = {r.output.name: Int(f'rcount_{r.output.name}') for r in recipes}
 
 zreactions = [ZReaction(r.output.name) for r in recipes]
 
-# Express outputs based on reaction count and inputs
-zr = zreactions[0]
-print(zr.input_zvar_counts)
-print(zr.output_zvar_count)
-print(zr.input_zvar_counts_dict)
+# Ensure counts are positive
+for zvar in rcounts.values():
+    s.add(zvar >= 0)
 
 # Represent total counts of each resource (ie: produced - consumed)
-net_res: dict[str, Any] = {name: 0 for name in z_res}
-for zreaction in zreactions:
+produced = {n: 0 for n in z_res}
+consumed = {n: 0 for n in z_res}
+# net_res: dict[str, Any] = {name: 0 for name in z_res}
 
+for zreaction in zreactions:
     # Add outputs
-    net_res[zreaction.reaction.output.name] += zreaction.output_zvar_count
+    produced[zreaction.reaction.output.name] += zreaction.output_zvar_count
 
     # Subtract inputs
     for name, expr in zreaction.input_zvar_counts_dict.items():
-        net_res[name] -= expr
+        consumed[name] += expr
 
-for k, v in net_res.items():
-    print('>>>', k)
-    print(v)
-    print()
+# Ensure resource consumption never exceeds production
+totals = {}
+for name in {*produced, *consumed}:
+    totals[name] = total = produced.get(name, 0) - consumed.get(name, 0)
+    s.add(total >= 0)
+
+# Specify goal condition
+s.add(z_res['FUEL'] == 1)
+
+assert s.check()
+
+# print('Minimizing')
+# s.minimize(rcounts['ORE'])
+# print('Minimized:', res)
+
+if m := s.model():
+    print(m[z_res['ORE']])
+    print(m.evaluate(produced['ORE']))
+    print(m.evaluate(rcounts['ORE']))
+else:
+    print('unsat')
