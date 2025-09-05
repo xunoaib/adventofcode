e45e32d393ee91b40b623760d00609097fdf911b
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-05 02:26:08 -0400

    Find distance to each point

---
+++ 2018/day06/day06.py
@@ -0,0 +1,45 @@
+import sys
+from collections import defaultdict
+
+
+def neighbors4(x, y):
+    yield (x - 1, y)
+    yield (x + 1, y)
+    yield (x, y - 1)
+    yield (x, y + 1)
+
+
+def explore(p: tuple[int, int]):
+    q = [(p, 0)]
+    distto = {p: 0}
+    while q:
+        p, dist = q.pop(0)
+        for n in neighbors4(*p):
+            if n[0] not in range(min_x, max_x
+                                 + 1) or n[1] not in range(min_y, max_y + 1):
+                continue
+            if n not in distto:
+                distto[n] = dist + 1
+                q.append((n, dist + 1))
+    return distto
+
+
+points = [tuple(map(int, line.split(', '))) for line in sys.stdin]
+
+min_x = min(x for x, y in points)
+min_y = min(y for x, y in points)
+max_x = max(x for x, y in points)
+max_y = max(y for x, y in points)
+
+dists = defaultdict(list)
+
+for i, (x, y) in enumerate(points):
+    distto = explore((x, y))
+
+    for p, dist in distto.items():
+        dists[p].append((dist, i))
+
+    # print(len(distto))
+    # dists.append(distto)
+
+print(dists[1, 1])
