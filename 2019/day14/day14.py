a8378096fe13843cde934d5234f6081504c0b63f
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-05 18:01:52 -0400

    Solve Day 14, Part 1, 2019

--- 2019/day14/day14.py
+++ 2019/day14/day14.py
@@ -3,7 +3,7 @@ from collections import Counter, defaultdict
 from dataclasses import dataclass
 from typing import Any
 
-from z3 import Int, Optimize, Solver
+from z3 import ArithRef, Int, Optimize, Solver
 
 
 @dataclass(frozen=True)
@@ -47,31 +47,15 @@ class ZReaction:
         return recipe_lookup[self.name]
 
     @property
-    def input_zvars(self):
-        return [z_res[inp.name] for inp in self.reaction.inputs]
-
-    @property
-    def input_zvar_counts(self):
-        return [
-            z_res[inp.name] * inp.amount * rcounts[self.name]
-            for inp in self.reaction.inputs
-        ]
-
-    @property
-    def input_zvar_counts_dict(self):
+    def zconsumed(self):
         return {
-            inp.name: z_res[inp.name] * inp.amount * rcounts[self.name]
+            inp.name: inp.amount * rcounts[self.name]
             for inp in self.reaction.inputs
         }
 
     @property
-    def output_zvar(self):
-        return z_res[self.name]
-
-    @property
-    def output_zvar_count(self):
-        return z_res[self.name
-                     ] * self.reaction.output.amount * rcounts[self.name]
+    def zproduced(self):
+        return self.reaction.output.amount * rcounts[self.name]
 
     @property
     def reaction_count_zvar(self):
@@ -80,49 +64,47 @@ class ZReaction:
 
 s = Optimize()
 
-# Create variables representing the total amount of each resource
-z_res = {r.output.name: Int(f'{r.output.name}') for r in recipes}
+names = [r.output.name for r in recipes]
 
 # Create variables representing the number of times each reaction occurs
 rcounts = {r.output.name: Int(f'rcount_{r.output.name}') for r in recipes}
 
-zreactions = [ZReaction(r.output.name) for r in recipes]
-
-# Ensure counts are positive
-for zvar in rcounts.values():
-    s.add(zvar >= 0)
-
 # Represent total counts of each resource (ie: produced - consumed)
-produced = {n: 0 for n in z_res}
-consumed = {n: 0 for n in z_res}
-# net_res: dict[str, Any] = {name: 0 for name in z_res}
+produced: dict[str, ArithRef | int] = {n: 0 for n in names}
+consumed: dict[str, ArithRef | int] = {n: 0 for n in names}
 
-for zreaction in zreactions:
+for r in recipes:
+    zreaction = ZReaction(r.output.name)
+
     # Add outputs
-    produced[zreaction.reaction.output.name] += zreaction.output_zvar_count
+    produced[zreaction.name] += zreaction.zproduced
 
     # Subtract inputs
-    for name, expr in zreaction.input_zvar_counts_dict.items():
+    for name, expr in zreaction.zconsumed.items():
         consumed[name] += expr
 
 # Ensure resource consumption never exceeds production
 totals = {}
 for name in {*produced, *consumed}:
-    totals[name] = total = produced.get(name, 0) - consumed.get(name, 0)
+    iproduced = produced.get(name, 0)
+    iconsumed = consumed.get(name, 0)
+    totals[name] = total = iproduced - iconsumed
     s.add(total >= 0)
+    s.add(iproduced >= 0)
+    s.add(iconsumed >= 0)
 
-# Specify goal condition
-s.add(z_res['FUEL'] == 1)
+# Ensure reaction counts are positive
+for rcount in rcounts.values():
+    s.add(rcount >= 0)
 
-assert s.check()
-
-# print('Minimizing')
-# s.minimize(rcounts['ORE'])
-# print('Minimized:', res)
+# Specify goal conditions
+s.add(produced['FUEL'] == 1)
+s.minimize(rcounts['ORE'])
+s.check()
 
 if m := s.model():
-    print(m[z_res['ORE']])
-    print(m.evaluate(produced['ORE']))
-    print(m.evaluate(rcounts['ORE']))
+    a1 = m.evaluate(produced['ORE'])
+    print('part1:', a1)
+    assert a1 == 720484
 else:
     print('unsat')
