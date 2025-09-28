0ede2b2d826ae536f713a5e5a523f329d72fc181
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 14:01:50 -0400

    Solve part 2

--- 2017/day06/day06.py
+++ 2017/day06/day06.py
@@ -19,3 +19,23 @@ while True:
     seen.add(t)
 
 print('part1:', a1)
+
+seen = {tuple(banks)}
+
+a1 = 0
+while True:
+    blocks, idx = max((bank, -idx) for idx, bank in enumerate(banks))
+    idx = abs(idx)
+    a1 += 1
+
+    banks[idx] = 0
+    for i in range(blocks):
+        banks[(idx + i + 1) % len(banks)] += 1
+
+    t = tuple(banks)
+    if t in seen:
+        break
+
+    seen.add(t)
+
+print('part2:', a1)
