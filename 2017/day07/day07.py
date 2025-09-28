2554a9b8dca3cf01be7836ab409c198d70a4fce9
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 04:38:12 -0400

    Always create holdings

--- 2017/day07/day07.py
+++ 2017/day07/day07.py
@@ -13,10 +13,12 @@ def parse_input(data: str):
         if m := re.match(r'^(.*?) \((.*)\) -> (.*)$', line):
             l = m.group(1)
             r = m.group(3)
+            holding[l]
             holding[r] |= set(r.split(', '))
             weights[l] = int(m.group(2))
         elif m := re.search(r'^(.*?) \((.*)\)', line):
             l = m.group(1)
+            holding[l]
             weights[l] = int(m.group(2))
 
     return weights, dict(holding)
@@ -24,6 +26,10 @@ def parse_input(data: str):
 
 def main():
     weights, holding = parse_input(sys.stdin.read())
+
+    for k, v in holding.items():
+        if not v:
+            print(k, v)
 
 
 if __name__ == '__main__':
