069e9c6ed824af4611dd0528a51b9cf9c50007d8
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 13:57:19 -0400

    Solve part 1

---
+++ 2017/day06/day06.py
@@ -0,0 +1,21 @@
+banks = list(map(int, input().split()))
+
+seen = {tuple(banks)}
+
+a1 = 0
+while True:
+    blocks, idx = max((bank, -idx) for idx, bank in enumerate(banks))
+    idx = abs(idx)
+    a1 += 1
+
+    banks[idx] = 0
+    for i in range(blocks):
+        banks[(idx + i + 1) % len(banks)] += 1
+
+    t = tuple(banks)
+    if t in seen:
+        break
+
+    seen.add(t)
+
+print('part1:', a1)
