83cb1e2ef030afccdf98ddc0fa36a6854b93a2e4
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 04:42:37 -0400

    Solve Part 1

--- 2017/day07/day07.py
+++ 2017/day07/day07.py
@@ -6,15 +6,17 @@ from collections import defaultdict
 def parse_input(data: str):
     lines = data.splitlines()
 
-    weights = {}
-    holding = defaultdict(set)
+    weights: dict[str, int] = {}
+    holding: dict[str, set[str]] = defaultdict(set)
+    depends_on = defaultdict(set)
 
     for line in lines:
         if m := re.match(r'^(.*?) \((.*)\) -> (.*)$', line):
             l = m.group(1)
-            r = m.group(3)
+            rs = m.group(3).split(', ')
             holding[l]
-            holding[r] |= set(r.split(', '))
+            for r in rs:
+                holding[r].add(l)
             weights[l] = int(m.group(2))
         elif m := re.search(r'^(.*?) \((.*)\)', line):
             l = m.group(1)
@@ -27,9 +29,9 @@ def parse_input(data: str):
 def main():
     weights, holding = parse_input(sys.stdin.read())
 
-    for k, v in holding.items():
-        if not v:
-            print(k, v)
+    a1 = next(k for k, v in holding.items() if not v)
+
+    print('part1:', a1)
 
 
 if __name__ == '__main__':
