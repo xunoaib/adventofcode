757d3f5e6e5b54947eccf6612ae60a0fda2a91b5
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 05:21:44 -0400

    Solve Day 7, Part 2, 2017

--- 2017/day07/day07.py
+++ 2017/day07/day07.py
@@ -1,6 +1,6 @@
 import re
 import sys
-from collections import defaultdict
+from collections import Counter, defaultdict
 from dataclasses import dataclass, field
 
 
@@ -10,13 +10,13 @@ class Disc:
     weight: int
     above: list['Disc'] = field(default_factory=list)
 
-    # @property
-    # def total_weight(self):
-    #     return self.weight + self.weight_above
+    @property
+    def total_weight(self):
+        return self.weight + self.weight_above
 
-    # @property
-    # def weight_above(self):
-    #     return sum(d.total_weight for d in self.above)
+    @property
+    def weight_above(self):
+        return sum(d.total_weight for d in self.above)
 
 
 def parse_input(data: str):
@@ -42,6 +42,31 @@ def parse_input(data: str):
     return weights, dict(holding)
 
 
+def walk_anomaly_tree(discs: dict[str, Disc], node):
+    hist = []
+    while True:
+        above = discs[node].above
+        ws = [d.total_weight for d in above]
+        counts = Counter(ws)
+        unique = [d for d in above if counts[d.total_weight] == 1]
+        expected = [d for d in above if counts[d.total_weight] > 1]
+
+        print()
+        print('above:  ', [d.id for d in above])
+        print('weights:', ws)
+        # print(uniques)
+
+        if len(unique) != 1:
+            break
+
+        w_e = expected[0].total_weight
+        w_u = unique[0].total_weight
+        hist.append((w_e, w_u, expected[0].id, unique[0].id, w_e - w_u))
+        node = unique[0].id
+
+    return hist
+
+
 def main():
     weights, holding = parse_input(sys.stdin.read())
     assert len(weights) == len(holding)
@@ -55,7 +80,14 @@ def main():
         for l in ls:
             discs[l].above.append(discs[r])
 
-    print([x.id for x in discs[a1].above])
+    hist = walk_anomaly_tree(discs, a1)
+
+    we, wu, eid, uid, diff = hist[-1]
+    a2 = discs[uid].weight + diff
+
+    print('part2:', a2)
+
+    assert a2 == 362
 
 
 if __name__ == '__main__':
