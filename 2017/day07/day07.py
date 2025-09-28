fb2a639733ea858e44aa52b2f42d2252ee18a894
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 04:57:15 -0400

    Construct disc objects

--- 2017/day07/day07.py
+++ 2017/day07/day07.py
@@ -1,6 +1,22 @@
 import re
 import sys
 from collections import defaultdict
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Disc:
+    id: str
+    weight: int
+    above: list['Disc'] = field(default_factory=list)
+
+    # @property
+    # def total_weight(self):
+    #     return self.weight + self.weight_above
+
+    # @property
+    # def weight_above(self):
+    #     return sum(d.total_weight for d in self.above)
 
 
 def parse_input(data: str):
@@ -28,10 +44,18 @@ def parse_input(data: str):
 
 def main():
     weights, holding = parse_input(sys.stdin.read())
+    assert len(weights) == len(holding)
 
     a1 = next(k for k, v in holding.items() if not v)
-
     print('part1:', a1)
+
+    discs = {n: Disc(n, w) for n, w in weights.items()}
+
+    for r, ls in holding.items():
+        for l in ls:
+            discs[l].above.append(discs[r])
+
+    print([x.id for x in discs[a1].above])
 
 
 if __name__ == '__main__':
