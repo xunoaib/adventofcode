cd46799a2a591517e8ed5d71c0246602be46f4a6
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 22:59:54 -0400

    Simplify

--- 2016/day09/day09.py
+++ 2016/day09/day09.py
@@ -3,14 +3,12 @@ import re
 line = input()
 result = ''
 
-while True:
-    if m := re.search(r'\((\d+)x(\d+)\)', line):
-        nchar, nrepeat = map(int, m.groups())
-        result += line[:m.start()] + line[m.end():m.end() + nchar] * nrepeat
-        line = line[m.end() + nchar:]
-    else:
-        result += line
-        break
+while m := re.search(r'\((\d+)x(\d+)\)', line):
+    nchar, nrepeat = map(int, m.groups())
+    result += line[:m.start()] + line[m.end():m.end() + nchar] * nrepeat
+    line = line[m.end() + nchar:]
+
+result += line
 
 a1 = len(result)
 
