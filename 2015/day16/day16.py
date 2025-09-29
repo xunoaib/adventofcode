bc82f19ab9337d9735dc62a3fce4ad417dee94d2
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-29 02:52:46 -0400

    Solve part 2

--- 2015/day16/day16.py
+++ 2015/day16/day16.py
@@ -1,3 +1,4 @@
+import operator
 import sys
 
 
@@ -10,6 +11,32 @@ def part1():
         counts = {k: int(v) for k, v in counts}
 
         if all(v == legend[k] for k, v in counts.items()):
+            candidates.append(i + 1)
+
+    assert len(candidates) == 1
+
+    return candidates[0]
+
+
+def part2():
+    candidates: list[int] = []
+
+    operators = {
+        'cats': operator.gt,
+        'trees': operator.gt,
+        'pomeranians': operator.lt,
+        'goldfish': operator.lt,
+    }
+
+    for i, line in enumerate(lines):
+        l, r = line.split(': ', 1)
+        counts = [g.split(': ') for g in r.split(', ')]
+        counts = {k: int(v) for k, v in counts}
+
+        if all(
+            operators.get(k, operator.eq)(v, legend[k])
+            for k, v in counts.items()
+        ):
             candidates.append(i + 1)
 
     assert len(candidates) == 1
@@ -33,5 +60,7 @@ legend = {
 lines = sys.stdin.read().splitlines()
 
 a1 = part1()
+a2 = part2()
 
 print('part1:', a1)
+print('part2:', a2)
