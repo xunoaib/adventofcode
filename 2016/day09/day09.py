58f2527c273ffacd2bbbd7c3b8f762a2f6d65418
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 23:00:43 -0400

    Extract into function

--- 2016/day09/day09.py
+++ 2016/day09/day09.py
@@ -1,15 +1,19 @@
 import re
 
+
+def part1(line: str):
+    result = ''
+
+    while m := re.search(r'\((\d+)x(\d+)\)', line):
+        nchar, nrepeat = map(int, m.groups())
+        result += line[:m.start()] + line[m.end():m.end() + nchar] * nrepeat
+        line = line[m.end() + nchar:]
+
+    return len(result + line)
+
+
 line = input()
-result = ''
 
-while m := re.search(r'\((\d+)x(\d+)\)', line):
-    nchar, nrepeat = map(int, m.groups())
-    result += line[:m.start()] + line[m.end():m.end() + nchar] * nrepeat
-    line = line[m.end() + nchar:]
-
-result += line
-
-a1 = len(result)
+a1 = part1(line)
 
 print('part1:', a1)
