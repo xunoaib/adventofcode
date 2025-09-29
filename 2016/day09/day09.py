e1374b39e15a6b6b8afa077db60a81532bd4b61b
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-28 23:08:49 -0400

    Solve Day 9, Part 2, 2016

--- 2016/day09/day09.py
+++ 2016/day09/day09.py
@@ -12,8 +12,30 @@ def part1(line: str):
     return len(result + line)
 
 
-line = input()
+def part2(line: str) -> int:
+    count = 0
 
-a1 = part1(line)
+    while m := re.search(r'\((\d+)x(\d+)\)', line):
+        nchar, nrepeat = map(int, m.groups())
+        s = line[m.end():m.end() + nchar]
+        count += len(line[:m.start()]) + part2(s) * nrepeat
+        line = line[m.end() + nchar:]
 
-print('part1:', a1)
+    return count + len(line)
+
+
+def main():
+    line = input()
+
+    a1 = part1(line)
+    a2 = part2(line)
+
+    print('part1:', a1)
+    print('part2:', a2)
+
+    assert a1 == 99145
+    assert a2 == 10943094568
+
+
+if __name__ == '__main__':
+    main()
