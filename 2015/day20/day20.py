ac50d430d3157ae25020d04c7e01beb54c32eafa
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-09-30 02:07:42 -0400

    Clean up

--- 2015/day20/day20.py
+++ 2015/day20/day20.py
@@ -1,19 +1,18 @@
-from collections import Counter
-
 N = int(input())
 MAXELF = N // 10 + 1  #??
 
 
-def part1(n):
-    presents = Counter()
+def part1():
+    presents = [0] * MAXELF
     for elf in range(1, MAXELF):
         for house in range(elf, MAXELF, elf):
             presents[house] += 10 * elf
-    return min(house for house, count in presents.items() if count >= n)
+
+    return next(house for house, count in enumerate(presents) if count >= N)
 
 
-def part2(n):
-    presents = Counter()
+def part2():
+    presents = [0] * MAXELF
     for elf in range(1, MAXELF):
         houseshit = 0
         for house in range(elf, MAXELF, elf):
@@ -22,11 +21,11 @@ def part2(n):
             if houseshit >= 50:
                 break
 
-    return min(house for house, count in presents.items() if count >= n)
+    return next(house for house, count in enumerate(presents) if count >= N)
 
 
-a1 = part1(N)
-a2 = part2(N)
+a1 = part1()
+a2 = part2()
 
 print('part1:', a1)
 print('part2:', a2)
