a893c64db2b6a074ee6a8868a3ccdee05cad06de
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-10-02 02:16:11 -0400

    Simplify

--- 2015/day25/day25.py
+++ 2015/day25/day25.py
@@ -4,16 +4,12 @@ tr, tc = map(int, re.findall(r'\d+', input()))
 
 a1 = 20151125
 r = c = 1
-nextrow = 2
 
 while (r, c) != (tr, tc):
     r -= 1
     c += 1
-
     if r < 1:
-        c = 1
-        r = nextrow
-        nextrow += 1
+        r, c = c, 1
 
     a1 = (a1 * 252533) % 33554393
 
