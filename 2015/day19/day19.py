2c00f7a9846c95c8e75e33779166e2b57f2e6cef
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-10-05 04:18:20 -0400

    Make globals uppercase

--- 2015/day19/day19.py
+++ 2015/day19/day19.py
@@ -1,16 +1,16 @@
 import re
 import sys
 
-repl_strs, input_str = sys.stdin.read().strip().split('\n\n')
-repls = [s.split(' => ') for s in repl_strs.splitlines()]
+REPL_STRS, INPUT_STR = sys.stdin.read().strip().split('\n\n')
+REPLS = [s.split(' => ') for s in REPL_STRS.splitlines()]
 
 
 def part1():
     distinct: set[str] = set()
 
-    for srch, repl in repls:
-        for m in re.finditer(srch, input_str):
-            s = input_str[:m.start()] + repl + input_str[m.end():]
+    for srch, repl in REPLS:
+        for m in re.finditer(srch, INPUT_STR):
+            s = INPUT_STR[:m.start()] + repl + INPUT_STR[m.end():]
             distinct.add(s)
 
     return len(distinct)
@@ -20,7 +20,7 @@ def part2():
     # pattern = r'^(.*)Rn(.*?)Ar(.*)$'
     # pattern = r'Rn(.*?)Ar'
     pattern = r'Rn((?:(?!Rn|Ar|\.\.\.).)*?)Ar'
-    s = input_str
+    s = INPUT_STR
 
     last_s = s
     while m := re.search(pattern, s):
@@ -41,8 +41,8 @@ def part2():
 
 
 def analyze():
-    left = set(c for sr in repls for c in sr[0])
-    right = set(c for sr in repls for c in sr[1])
+    left = set(c for sr in REPLS for c in sr[0])
+    right = set(c for sr in REPLS for c in sr[1])
 
     print('shared =', ''.join(sorted(left & right)))
     print('unique_r =', ''.join(sorted(right - left)))
