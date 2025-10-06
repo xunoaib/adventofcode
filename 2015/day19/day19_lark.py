4e769768c6851926a1d28369a6c76647febe24a7
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-10-05 22:48:29 -0400

    Parse grammar with Lark

---
+++ 2015/day19/day19_lark.py
@@ -0,0 +1,56 @@
+import re
+import sys
+from collections import defaultdict
+
+from lark import Lark, Tree
+
+
+def molecules(s: str):
+    return re.findall(r'[A-Z][a-z]?', s)
+
+
+REPL_STRS, INPUT_STR = sys.stdin.read().strip().split('\n\n')
+_REPLS = [s.split(' => ') for s in REPL_STRS.splitlines()]
+REPLS: list[tuple[str, str]] = [(l, r) for l, r in _REPLS]
+REV_REPLS = [(r, l) for l, r in REPLS]
+
+revs = defaultdict(set)
+exprs = defaultdict(set)
+
+for l, r in REPLS:
+    # revs[l].add(l)
+    revs[l].add(r)
+
+for l, rs in list(revs.items()):
+    for r in rs:
+        # s = ' '.join(f'"{m}"' for m in molecules(r))
+        s = ' '.join(f'{m.lower()}' for m in molecules(r))
+        exprs[r].add(s)
+        # revs[r].add(f'"{r}"')
+
+# for m in set(molecules(REPL_STRS)):
+#     revs[m.lower()].add(f'"{m}"')
+
+grammar = ''
+for r, ss in exprs.items():
+    grammar += f'{r.lower()} : {" | ".join(ss)}\n'
+
+grammar += '\n'
+for l, rs in revs.items():
+    grammar += f'{l.lower()} : "{l}" | {" | ".join(rs).lower()}\n'
+
+grammar += '\n'
+ms = set(molecules(REPL_STRS))
+for m in ms:
+    if m not in set(exprs) | set(revs):
+        grammar += f'{m.lower()} : "{m}"\n'
+print(grammar)
+
+parser = Lark(grammar, start="e")
+
+tree = parser.parse(INPUT_STR)
+print(tree.pretty())
+print(dir(tree))
+
+print(tree.data)
+print(tree.children)
