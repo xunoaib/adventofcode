bde64f13f431cb1f676cba2d988d356f479340ad
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-10-12 02:01:15 -0400

    Hopefully interpret deal inc correctly

--- 2019/day22/day22.py
+++ 2019/day22/day22.py
@@ -12,26 +12,12 @@ for line in lines:
         cards = cards[:n] + cards[n:]
     elif line.startswith('deal with increment'):
         n = int(line.split()[-1])
-        spots: list[int | None] = [None] * len(cards)
+        new_cards: list[int | None] = [None] * len(cards)
         pos = 0
-
-        print('Dealing with increment:', n)
-        print('Looping')
-
         while cards:
-            moves_left = n
-            while moves_left and None in spots:
-                pos = (pos + 1) % len(cards)
-                if spots[pos] is None:
-                    moves_left -= 1
-            spots[pos] = cards.pop(0)
-            print(
-                'writing spot', pos, 'cards left:', len(cards),
-                sum(s == None for s in spots)
-            )
-            # print(n, len(cards))
+            new_cards[pos] = cards.pop(0)
+            pos = (pos + 1) % len(new_cards)
 
-        cards = spots
-        print('Done')
-        # print(spots)
-        exit()
+        cards = new_cards
+
+print(cards)
