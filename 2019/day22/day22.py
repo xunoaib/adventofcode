54e17d2dde5454bce2ef606f50ead46b60478587
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-10-12 02:07:24 -0400

    Solve sample inputs

--- 2019/day22/day22.py
+++ 2019/day22/day22.py
@@ -2,6 +2,7 @@ import sys
 
 lines = sys.stdin.read().strip().splitlines()
 cards: list[int] = list(range(10007))
+cards: list[int] = list(range(10))
 
 for line in lines:
 
@@ -9,15 +10,15 @@ for line in lines:
         cards = cards[::-1]
     elif line.startswith('cut'):
         n = int(line.split()[-1])
-        cards = cards[:n] + cards[n:]
+        cards = cards[n:] + cards[:n]
     elif line.startswith('deal with increment'):
         n = int(line.split()[-1])
         new_cards: list[int | None] = [None] * len(cards)
-        pos = 0
+        src = tar = 0
         while cards:
-            new_cards[pos] = cards.pop(0)
-            pos = (pos + 1) % len(new_cards)
-
+            src = (src + n) % len(cards)
+            new_cards[tar] = cards.pop(0)
+            tar = (tar + n) % len(new_cards)
         cards = new_cards
 
 print(cards)
