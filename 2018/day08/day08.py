894068762d497d86a817fff39bbd821d983320ec
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-24 17:28:54 -0500

    Solve part 1

--- 2018/day08/day08.py
+++ 2018/day08/day08.py
@@ -11,6 +11,8 @@ class Node:
 
 
 def parse():
+    global tot
+
     n_children = vs.pop(0)
     n_metadata = vs.pop(0)
 
@@ -19,12 +21,14 @@ def parse():
         children.append(parse())
 
     metadata = [vs.pop(0) for _ in range(n_metadata)]
+    tot += sum(metadata)
     return Node(children, metadata)
 
 
 vs = list(map(int, input().split()))
 vs_orig = list(vs)
+tot = 0
 
 n = parse()
 
-print(n)
+print('part1:', tot)
