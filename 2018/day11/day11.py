13c6e0c790c3b4d9ceda84735f67e81365b47584
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 04:31:06 -0500

    Create functions

--- 2018/day11/day11.py
+++ 2018/day11/day11.py
@@ -30,37 +30,40 @@ def region_power(x, y, gsn, size):
     return base_power + xsum + ysum + corner
 
 
-gsn = int(input())
+def part1():
+    best = (float('-inf'), ) * 3
 
-# gsn = 18  # FIXME:
-# print(region_power(33, 45, gsn, 3))
-# exit()
+    for y in range(1, ROWS):
+        for x in range(1, COLS):
+            p = region_power(x, y, gsn, 3)
+            best = max(best, (p, x, y))
+
+    return f'{best[1]},{best[2]}'
+
+
+def part2():
+    best = (float('-inf'), ) * 4
+    a2 = None
+
+    for size in range(3, 300):
+        for y in range(1, ROWS - size):
+            for x in range(1, COLS - size):
+                p = region_power(x, y, gsn, size)
+                if p > best[0]:
+                    best = (p, x, y, size)
+                    a2 = f'{x},{y},{size}'
+                    print(f'new best {a2} with power {best[0]}')
+    return a2
+
+
+gsn = int(input())
 
 ROWS = COLS = 300
 
-best = (float('-inf'), ) * 3
-
-for y in range(1, ROWS):
-    for x in range(1, COLS):
-        p = region_power(x, y, gsn, 3)
-        best = max(best, (p, x, y))
-
-p, x, y = best
-a1 = f'{x},{y}'
+a1 = part1()
 print('part1:', a1)
 assert a1 == '235,16'
 
-best = (float('-inf'), ) * 4
-a2 = None
-
-for size in range(3, 300):
-    for y in range(1, ROWS - size):
-        for x in range(1, COLS - size):
-            p = region_power(x, y, gsn, size)
-            if p > best[0]:
-                best = (p, x, y, size)
-                a2 = f'{x},{y},{size}'
-                print('new best', a2, best[0])
-
-# NOTE: this answer is found quickly but the full search is very inefficient
+a2 = part2()
+print('part2:', a2)
 assert a2 == '236,227,14'
