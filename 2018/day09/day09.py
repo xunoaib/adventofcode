a6aafb8025a82671fe4606d2d86dad8d26b67182
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 01:34:06 -0500

    Add naive approach to part 2

--- 2018/day09/day09.py
+++ 2018/day09/day09.py
@@ -3,35 +3,44 @@ from collections import Counter
 
 num_players, last_num = map(int, re.findall(r'\d+', input()))
 
-scores = Counter()
 
-player_turn = 1
-circle = [0]
-cur_idx = 0
-marble_num = 1
+def part1(last_num: int):
+    scores = Counter()
 
-while marble_num < last_num:
+    player_turn = 1
+    circle = [0]
+    cur_idx = 0
+    marble_num = 1
 
-    # ns = ''.join(
-    #     [
-    #         f' \033[93m{"("+str(v)+")":>4}\033[0m'
-    #         if i == cur_idx else f' {v:>4}' for i, v in enumerate(circle)
-    #     ][::-1]
-    # )
-    # print(f'[{player_turn}] {ns}')
+    while marble_num < last_num:
 
-    if marble_num % 23 == 0:
-        scores[player_turn] += marble_num
-        i = (cur_idx + 7) % len(circle)
-        scores[player_turn] += circle.pop(i)
-        cur_idx = (i - 1) % len(circle)
-    else:
-        cur_idx = i = (cur_idx - 1) % len(circle)
-        circle.insert(i, marble_num)
+        # ns = ''.join(
+        #     [
+        #         f' \033[93m{"("+str(v)+")":>4}\033[0m'
+        #         if i == cur_idx else f' {v:>4}' for i, v in enumerate(circle)
+        #     ][::-1]
+        # )
+        # print(f'[{player_turn}] {ns}')
 
-    marble_num += 1
-    player_turn = (player_turn + 1) % num_players
+        if marble_num % 23 == 0:
+            scores[player_turn] += marble_num
+            i = (cur_idx + 7) % len(circle)
+            scores[player_turn] += circle.pop(i)
+            cur_idx = (i - 1) % len(circle)
+        else:
+            cur_idx = i = (cur_idx - 1) % len(circle)
+            circle.insert(i, marble_num)
 
-a1 = max(scores.values())
+        marble_num += 1
+        player_turn = (player_turn + 1) % num_players
+
+    return max(scores.values())
+
+
+a1 = part1(last_num)
+a2 = part1(last_num * 100)
 
 print('part1:', a1)
+print('part2:', a2)
+
+assert a1 == 373597
