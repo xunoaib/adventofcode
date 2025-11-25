fbebc305016d2fea66db38aa894a5c55464bc93d
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 03:20:27 -0500

    Solve part 1

--- 2018/day10/day10.py
+++ 2018/day10/day10.py
@@ -26,10 +26,27 @@ for line in sys.stdin:
     obj = px, py, vx, vy = list(map(int, re.findall(r'-?\d+', line)))
     objects.append(obj)
 
+
+def cost_of(objects):
+    minx = min(x for x, y, vx, vy in objects)
+    miny = min(y for x, y, vx, vy in objects)
+    maxx = max(x for x, y, vx, vy in objects)
+    maxy = max(y for x, y, vx, vy in objects)
+    return maxx - minx + maxy - miny
+
+
+best_cost = cost_of(objects)
+best_objects = objects
+while best_cost > 70:
+    objects = simulate(objects)
+    ncost = cost_of(objects)
+    if ncost < best_cost:
+        best_cost = ncost
+        best_objects = objects
+        print('new best', ncost)
+
 display()
-objects = simulate(objects)
-display()
-objects = simulate(objects)
-display()
-objects = simulate(objects)
-display()
+
+a1 = 'FPRBRRZA'
+
+print('part1:', a1)
