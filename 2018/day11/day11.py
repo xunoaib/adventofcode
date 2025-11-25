f6c0a4ef132e0dbaeedbd7764cf4cecb2887082d
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 04:43:38 -0500

    Remove old code

--- 2018/day11/day11.py
+++ 2018/day11/day11.py
@@ -1,39 +1,18 @@
-from functools import cache
-
 import numpy as np
 
 
-@cache
-def power_level(x, y, gsn):
+def power_level(x, y):
     rack_id = x + 10
     p = rack_id * y
-    p += gsn
+    p += GSN
     p *= rack_id
     p = ((p % 1000) - (p % 100)) // 100
     p -= 5
     return p
 
 
-@cache
-def region_power(x, y, gsn, size):
-    if size == 1:
-        return power_level(x, y, gsn)
-
-    base_power = region_power(x, y, gsn, size - 1)
-
-    xsum = sum(
-        power_level(xx, y + size - 1, gsn) for xx in range(x, x + size - 1)
-    )
-    ysum = sum(
-        power_level(x + size - 1, yy, gsn) for yy in range(y, y + size - 1)
-    )
-    corner = power_level(x + size - 1, y + size - 1, gsn)
-
-    return base_power + xsum + ysum + corner
-
-
 def part1():
-    best = best_power_at_size(gsn, 3)
+    best = best_power_at_size(3)
     return f'{best[1]},{best[2]}'
 
 
@@ -41,7 +20,7 @@ def part2():
     best = (float('-inf'), ) * 4
 
     for size in range(1, 300):
-        sbest = best_power_at_size(gsn, size)
+        sbest = best_power_at_size(size)
         best = max(best, sbest)
         if sbest == best:
             print(f'new best {best} with power {best[0]}')
@@ -50,7 +29,7 @@ def part2():
     return f'{x},{y},{size}'
 
 
-def best_power_at_size(gsn: int, size: int):
+def best_power_at_size(size: int):
     best = (float('-inf'), ) * 4
     for y in range(1, ROWS - size):
         for x in range(1, COLS - size):
@@ -59,12 +38,12 @@ def best_power_at_size(gsn: int, size: int):
     return best
 
 
-gsn = int(input())
+GSN = int(input())
 
 ROWS = COLS = 300
 
 COST_MATRIX = np.array(
-    [[power_level(x, y, gsn) for x in range(COLS)] for y in range(ROWS)]
+    [[power_level(x, y) for x in range(COLS)] for y in range(ROWS)]
 )
 
 a1 = part1()
