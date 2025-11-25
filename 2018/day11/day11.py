b25c8e382257f1a42403a981b03035fc87898edc
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 04:01:40 -0500

    Solve part 1

--- 2018/day11/day11.py
+++ 2018/day11/day11.py
@@ -1,7 +1,4 @@
-GSN = int(input())
-
-
-def power_level(x, y, gsn=GSN):
+def power_level(x, y, gsn):
     rack_id = x + 10
     p = rack_id * y
     p += gsn
@@ -11,7 +8,28 @@ def power_level(x, y, gsn=GSN):
     return p
 
 
-print(power_level(3, 5, 8))
-print(power_level(122, 79, 57))
-print(power_level(217, 196, 39))
-print(power_level(101, 153, 71))
+def region_power(x, y, gsn):
+    tot = 0
+    for xoff in range(3):
+        for yoff in range(3):
+            tot += power_level(x + xoff, y + yoff, gsn)
+    return tot
+
+
+gsn = int(input())
+best = (float('-inf'), ) * 3
+
+ROWS = COLS = 300
+
+for y in range(1, ROWS):
+    for x in range(1, COLS):
+        p = region_power(x, y, gsn)
+        best = max(best, (p, x, y))
+
+print(best)
+
+p, x, y = best
+
+a1 = f'{x},{y}'
+
+print('part1:', a1)
