965a4de26a0ea36457b7066af33b2729834f38ed
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 04:12:56 -0500

    Solve part 2 (hackily)

--- 2018/day11/day11.py
+++ 2018/day11/day11.py
@@ -1,3 +1,7 @@
+from functools import cache
+
+
+@cache
 def power_level(x, y, gsn):
     rack_id = x + 10
     p = rack_id * y
@@ -8,10 +12,10 @@ def power_level(x, y, gsn):
     return p
 
 
-def region_power(x, y, gsn):
+def region_power(x, y, gsn, size=3):
     tot = 0
-    for xoff in range(3):
-        for yoff in range(3):
+    for xoff in range(size):
+        for yoff in range(size):
             tot += power_level(x + xoff, y + yoff, gsn)
     return tot
 
@@ -31,3 +35,18 @@ a1 = f'{x},{y}'
 print('part1:', a1)
 
 assert a1 == '235,16'
+
+best = (float('-inf'), ) * 4
+a2 = None
+
+for size in range(3, 300):
+    for y in range(1, ROWS - size):
+        for x in range(1, COLS - size):
+            p = region_power(x, y, gsn, size)
+            if p > best[0]:
+                best = (p, x, y, size)
+                a2 = f'{x},{y},{size}'
+                print('new best', best, a2)
+
+# NOTE: this answer is found quickly but the full search is very inefficient
+assert a2 == '236,227,14'
