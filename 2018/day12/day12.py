b62640ec4599e7c98d4a8e01c0f17c23bf22cfd7
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 05:42:50 -0500

    Solve part 1

--- 2018/day12/day12.py
+++ 2018/day12/day12.py
@@ -8,30 +8,32 @@ rules = dict(batched(pairs, 2))
 
 
 def step(state: str, idx: int):
-    if not state.startswith('....'):
-        state = '....' + state
-        idx -= 4
-    if not state.endswith('....'):
-        state += '....'
+    while not state.startswith('....'):
+        state = '.' + state
+        idx += 1
+
+    while not state.endswith('....'):
+        state += '.'
 
     vals = ['.'] * len(state)
 
     for i in range(len(state)):
-        seg = state[i:i + 5]
+        seg = state[i - 2:i + 3]
         vals[i] = rules.get(seg, vals[i])
 
     return ''.join(vals).rstrip('.'), idx
 
 
 def pot_nums(state, idx):
-    return [idx for idx, v in enumerate(state, start=idx) if v == '#']
+    return [idx for idx, v in enumerate(state, start=-idx) if v == '#']
 
 
-gens = [(f'....{state}....', -2)]
+gens = [(f'....{state}....', 4)]
 for _ in range(20):
     gens.append(step(*gens[-1]))
 
 for i, (s, idx) in enumerate(gens):
-    # print(f'{i>2}: {s.ljust(".")}')
     print(f'{i:>2}: ({idx:>3}) {s}')
     print(pot_nums(s, idx))
+
+print('part1:', sum(pot_nums(*gens[-1])))
