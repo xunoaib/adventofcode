aca5216d81861badb2d4b3109651ad0a9c1ca8a2
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-25 05:29:11 -0500

    Try tracking pot numbers

--- 2018/day12/day12.py
+++ 2018/day12/day12.py
@@ -7,11 +7,12 @@ state, *pairs = re.findall(r'[#.]+', sys.stdin.read())
 rules = dict(batched(pairs, 2))
 
 
-def step(state: str):
-    if not state.startswith('..'):
-        state = '..' + state
-    if not state.endswith('..'):
-        state += '..'
+def step(state: str, idx: int):
+    if not state.startswith('....'):
+        state = '....' + state
+        idx -= 4
+    if not state.endswith('....'):
+        state += '....'
 
     vals = ['.'] * len(state)
 
@@ -19,16 +20,13 @@ def step(state: str):
         seg = state[i:i + 5]
         vals[i] = rules.get(seg, vals[i])
 
-    # exit()
-
-    return '....' + ''.join(vals).strip('.') + '....'
+    return ''.join(vals).rstrip('.'), idx
 
 
-state = f'....{state}....'
-gens = [state]
+gens = [(f'....{state}....', -4)]
 for _ in range(20):
-    gens.append(state := step(state))
+    gens.append(step(*gens[-1]))
 
-for i, s in enumerate(gens):
+for i, (s, idx) in enumerate(gens):
     # print(f'{i>2}: {s.ljust(".")}')
-    print(f'{i:>2}: {s}')
+    print(f'{i:>2}: ({idx:>3}) {s}')
