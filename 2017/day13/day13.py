b53c5c4c85ea518d63771d75df02b16762eda010
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-27 22:39:36 -0500

    Refactor part 1 into function

--- 2017/day13/day13.py
+++ 2017/day13/day13.py
@@ -4,13 +4,20 @@ lines = sys.stdin.read().strip().split('\n')
 
 ranges: dict[int, int] = dict(tuple(map(int, l.split(': '))) for l in lines)
 
-a1 = 0
 
-for d in range(max(ranges) + 1):
-    if r := ranges.get(d):
-        pos = d % (2 * r - 2)
-        if pos == 0:
-            a1 += d * r
+def part1(delay: int):
+
+    sev = 0
+    for d in range(max(ranges) + 1):
+        d += delay
+        if r := ranges.get(d):
+            pos = d % (2 * r - 2)
+            if pos == 0:
+                sev += d * r
+    return sev
+
+
+a1 = part1(0)
 
 print('part1:', a1)
 
