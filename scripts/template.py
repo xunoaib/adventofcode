734911c94c0b41b05be263c30cab7d6991656df2
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-28 00:43:57 -0500

    Update Python template

--- scripts/template.py
+++ scripts/template.py
@@ -1,39 +1,25 @@
-#!/usr/bin/env python3
-
 import sys
 from collections import Counter, defaultdict
 from heapq import heappop, heappush
 from itertools import pairwise, permutations, product
 
-DIRS = U, R, D, L = (-1, 0), (0, 1), (1, 0), (0, -1)
+aa = bb = None
 
+s = sys.stdin.read()
+vs = list(tuple(map(int, s.split(','))))
+lines = s.strip().split('\n')
 
-def neighbors8(r, c):
-    for roff, coff in product([-1, 0, 1], repeat=2):
-        if not (roff and coff):
-            yield r + roff, c + coff
+# grid = {
+#     (r, c): ch
+#     for r, line in enumerate(lines)
+#     for c, ch in enumerate(line)
+# }
 
-def neighbors4(r, c):
-    for roff, coff in DIRS:
-        if not (roff and coff):
-            yield r + roff, c + coff
+if locals().get('aa') is not None:
+    print('part1:', aa)
 
+if locals().get('bb') is not None:
+    print('part2:', bb)
 
-lines = sys.stdin.read().strip().split('\n')
-
-for line in lines:
-    print(line)
-
-grid = {
-    (r, c): ch
-    for r, line in enumerate(lines)
-    for c, ch in enumerate(line)
-}
-
-a1 = a2 = 0
-
-# print('part1:', a1)
-# print('part2:', a2)
-
-# assert a1 == 0
-# assert a2 == 0
+# assert aa == 0
+# assert bb == 0
