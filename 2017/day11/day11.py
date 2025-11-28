9a441149326ebfa35221118d2b5ffcd3e060e610
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-27 22:01:27 -0500

    Solve part 2

--- 2017/day11/day11.py
+++ 2017/day11/day11.py
@@ -1,22 +1,43 @@
 from collections import Counter
 
+ORDERS = 'n ne se s sw nw'.split() * 2
+
+
+def measure_dist(ds):
+    c = Counter(ds)
+
+    for a, b in [('ne', 'sw'), ('nw', 'se'), ('s', 'n')]:
+        if c[b] < c[a]:
+            a, b = b, a
+        c[b] -= c[a]
+        c[a] = 0
+
+    def combine(a, b, into):
+        # cancels out moves (nw + ne become n)
+        if c[b] < c[a]:
+            a, b = b, a
+
+        c[into] += c[a]
+        c[b] -= c[a]
+        c[a] = 0
+
+    for i in range(6):
+        a, into, b = ORDERS[i:i + 3]
+        combine(a, b, into)
+
+    return sum(c.values())
+
+
 ds = input().split(',')
 
-c = Counter(ds)
+a1 = a2 = 0
 
-for a, b in [('ne', 'sw'), ('nw', 'se'), ('s', 'n')]:
-    if c[b] < c[a]:
-        a, b = b, a
-    c[b] -= c[a]
-    c[a] = 0
+for i in range(len(ds)):
+    a1 = measure_dist(ds[:i + 1])
+    a2 = max(a2, a1)
 
-a, b = 'ne', 'nw'
+print('part1:', a1)
+print('part2:', a2)
 
-if c[b] < c[a]:
-    a, b = b, a
-
-c['n'] += c[a]  # add nw + ne == n
-c[b] -= c[a]
-c[a] = 0
-
-print('part1:', c['n'] + c['ne'])
+assert a1 == 764
+assert a2 == 1532
