35177a89ca26f5755ca2ed79a501773b7ca524dd
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-11-27 22:49:09 -0500

    Solve part 2

--- 2017/day13/day13.py
+++ 2017/day13/day13.py
@@ -5,20 +5,27 @@ lines = sys.stdin.read().strip().split('\n')
 ranges: dict[int, int] = dict(tuple(map(int, l.split(': '))) for l in lines)
 
 
-def part1(delay: int):
-
+def severity(delay: int):
     sev = 0
+    caught = False
+
     for d in range(max(ranges) + 1):
-        d += delay
         if r := ranges.get(d):
-            pos = d % (2 * r - 2)
-            if pos == 0:
+            if (d + delay) % (2 * r - 2) == 0:
                 sev += d * r
-    return sev
+                caught = True
+
+    return sev if caught else None
 
 
-a1 = part1(0)
+a1 = severity(0)
+
+delay = 0
+while (sev := severity(delay)) is not None:
+    delay += 1
 
 print('part1:', a1)
+print('part2:', delay)
 
 assert a1 == 1728
+assert a2 == 3946838
