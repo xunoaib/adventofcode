c70851291f894fdc48d4cdc310bf3ca1ccb8b74a
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-03 00:49:22 -0500

    Clean up

--- 2025/day03/day03.py
+++ 2025/day03/day03.py
@@ -1,63 +1,33 @@
 import sys
-from collections import Counter, defaultdict
-from heapq import heappop, heappush
-from itertools import combinations, pairwise, permutations, product
-
-aa = bb = None
+from itertools import combinations
 
 lines = sys.stdin.read().strip().split('\n')
 
-aa = bb = 0
+a1 = a2 = 0
 
 for line in lines:
     xs = list(map(int, list(line)))
-
     m = 0
     for a, b in combinations(xs, r=2):
-        m = max(m, int(f'{a}{b}'))
-
-    aa += m
+        m = max(m, a * 10 + b)
+    a1 += m
 
 for line in lines:
     xs = list(map(int, list(line)))
-
-    vals = [(v, i) for i, v in enumerate(xs)]
-    vals.sort(key=lambda vi: (-vi[0], vi[1]))
-    left = 12
-
-    print()
-    print('>>> Line', line)
-    print()
+    vals = sorted(enumerate(xs), key=lambda iv: (-iv[1], iv[0]))
 
     s = []
-    cur_idx = None
-    while left:
-        # if not vals:
-        #     s += xs[idx:idx + left]
-        #     break
-
-        for v, i in vals:
-            print(vals)
+    for left in range(12, 0, -1):
+        for i, v in vals:
             if i <= len(xs) - left:
-                print(f'popping {v} @ {i}')
-                left -= 1
-                cur_idx = i
                 s.append(v)
-                vals = [(vv, ii) for vv, ii in vals if ii > cur_idx]
+                vals = [(ii, vv) for ii, vv in vals if ii > i]
                 break
-        else:
-            print('nothing found', s, vals)
-            exit()
 
-    s = ''.join(map(str, s))
-    print(s)
-    bb += int(s)
+    a2 += int(''.join(map(str, s)))
 
-if locals().get('aa') is not None:
-    print('part1:', aa)
+print('part1:', a1)
+print('part2:', a2)
 
-if locals().get('bb') is not None:
-    print('part2:', bb)
-
-# assert aa == 17301
-assert bb == 3121910778619
+assert a1 == 17301
+assert a2 == 172162399742349
