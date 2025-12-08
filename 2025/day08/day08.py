cd0b98085ff9e9e6b7b3812db4237f97739ab08d
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-08 04:34:57 -0500

    Combine parts 1 and 2

--- 2025/day08/day08.py
+++ 2025/day08/day08.py
@@ -3,36 +3,6 @@ from collections import defaultdict
 from math import prod
 
 
-def part1(dists):
-    circuits = defaultdict(set)
-
-    for _ in range(N):
-        _, p, q = dists.pop()
-
-        points1 = circuits.get(p, set())
-        points2 = circuits.get(q, set())
-        newpoints = points1 | points2 | {p, q}
-        circuits |= {r: newpoints for r in newpoints}
-
-    ids = {id(v): v for v in circuits.values()}
-    return prod(sorted(len(v) for v in ids.values())[-3:])
-
-
-def part2(dists):
-    circuits = defaultdict(set)
-
-    while True:
-        _, p, q = dists.pop()
-
-        points1 = circuits.get(p, set())
-        points2 = circuits.get(q, set())
-        newpoints = points1 | points2 | {p, q}
-        circuits |= {r: newpoints for r in newpoints}
-
-        if len(newpoints) == len(points):
-            return p[0] * q[0]
-
-
 def dist(p, q):
     return sum((i - j)**2 for i, j in zip(p, q))
 
@@ -44,13 +14,26 @@ dists = []
 for i, p in enumerate(points):
     for q in points[i + 1:]:
         dists.append((dist(p, q), p, q))
+dists.sort()
 
-dists.sort(reverse=True)
+circuits = defaultdict(set)
 
 N = 1000 if len(points) > 100 else 10
 
-aa = part1(dists.copy())
-bb = part2(dists.copy())
+for i, (_, p, q) in enumerate(dists):
+    points1 = circuits.get(p, set())
+    points2 = circuits.get(q, set())
+    newpoints = points1 | points2 | {p, q}
+    circuits |= {r: newpoints for r in newpoints}
+
+    if i == N - 1:
+        ids = {id(v): v for v in circuits.values()}
+        aa = prod(sorted(len(v) for v in ids.values())[-3:])
+
+
+    if len(newpoints) == len(points):
+        bb = p[0] * q[0]
+        break
 
 print('part1:', aa)
 print('part2:', bb)
