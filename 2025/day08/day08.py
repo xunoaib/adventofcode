7d0c442a2a0982cc6209fb2e98d708930382086e
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-08 00:38:55 -0500

    Solve part 2 and clean up

--- 2025/day08/day08.py
+++ 2025/day08/day08.py
@@ -1,7 +1,5 @@
 import sys
-from collections import Counter, defaultdict
-from heapq import heappop, heappush
-from itertools import pairwise, permutations, product
+from collections import defaultdict
 from math import prod
 
 
@@ -9,70 +7,66 @@ def dist(p, q):
     return sum(abs(i - j)**2 for i, j in zip(p, q))
 
 
-aa = bb = None
+def part1(dists):
+    circuit_points = defaultdict(set)
 
-s = sys.stdin.read()
-lines = s.strip().split('\n')
+    for _ in range(N):
+        _, p, q = dists.pop()
 
-rows = []
-for line in lines:
-    rows.append(tuple(map(int, line.split(','))))
+        points1 = circuit_points.get(p, set())
+        points2 = circuit_points.get(q, set())
+        newpoints = points1 | points2 | {p, q}
 
-distmap = {}
+        for r in newpoints:
+            if r in circuit_points:
+                del circuit_points[r]
+
+        for r in newpoints:
+            circuit_points[r] = newpoints
+
+    ids = {id(v): v for v in circuit_points.values()}
+    sizes = sorted(len(v) for v in ids.values())[-3:]
+    return prod(sizes)
+
+
+def part2(dists):
+    circuit_points = defaultdict(set)
+
+    while True:
+        _, p, q = dists.pop()
+
+        points1 = circuit_points.get(p, set())
+        points2 = circuit_points.get(q, set())
+        newpoints = points1 | points2 | {p, q}
+
+        for r in newpoints:
+            if r in circuit_points:
+                del circuit_points[r]
+
+        for r in newpoints:
+            circuit_points[r] = newpoints
+
+        if len(newpoints) == len(points):
+            return p[0] * q[0]
+
+
+lines = sys.stdin.read().strip().split('\n')
+points = [tuple(map(int, line.split(','))) for line in lines]
+
 dists = []
-for i, p in enumerate(rows):
-    for j, q in enumerate(rows):
-        if i < j:
-            dists.append((dist(p, q), p, q))
+for i, p in enumerate(points):
+    for q in points[i + 1:]:
+        dists.append((dist(p, q), p, q))
+
 dists.sort(reverse=True)
 
-circuit_points = defaultdict(set)  # point => set(connected_points)
+N = 1000 if len(points) > 100 else 10
 
-n = 1000 if len(rows) > 100 else 10
+aa = part1(dists.copy())
+bb = part2(dists.copy())
 
-for _ in range(n):
-    d, p, q = dists.pop()
+print('part1:', aa)
+print('part2:', bb)
 
-    points1 = circuit_points.get(p, set())
-    points2 = circuit_points.get(q, set())
-
-    newpoints = points1 | points2 | {p, q}
-
-    for r in newpoints:
-        if r in circuit_points:
-            del circuit_points[r]
-
-    for r in newpoints:
-        circuit_points[r] = newpoints
-
-ids = {id(v): v for v in circuit_points.values()}
-
-for i, v in ids.items():
-    print(i, len(v))
-
-sizes = sorted([len(v) for v in ids.values()])[-3:]
-print(sizes)
-aa = prod(sizes)
-
-# for k, v in circuit_points.items():
-#     i = id(v)
-#     if i in ids:
-#     print(k, len(v), )
-
-# while dists:
-#
-
-# grid = {
-#     (r, c): ch
-#     for r, line in enumerate(lines)
-#     for c, ch in enumerate(line)
-# }
-
-if locals().get('aa') is not None:
-    print('part1:', aa)
-
-if locals().get('bb') is not None:
-    print('part2:', bb)
-
-# assert aa == 0
-# assert bb == 0
+assert aa == 83520
+assert bb == 1131823407
