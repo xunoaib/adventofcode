24c404075b855728c1391224107b37880a024952
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-10 00:51:44 -0500

    Clean up

--- 2025/day10/day10.py
+++ 2025/day10/day10.py
@@ -5,70 +5,66 @@ from z3 import Int, Optimize, sat
 lines = sys.stdin.read().strip().split('\n')
 
 
+def neighbors(buttons, state):
+    for bs in buttons:
+        s = list(state)
+        for b in bs:
+            s[b] = not s[b]
+        yield tuple(s)
+
+
 class Machine:
 
     def __init__(self, line):
         lights, *buttons, req = [g[1:-1] for g in line.split(' ')]
-        self.goal = tuple(ch == '#' for ch in lights)
         self.buttons = tuple(tuple(map(int, g.split(','))) for g in buttons)
-        self.state = (False, ) * len(self.goal)
+        self.goal_lights = tuple(ch == '#' for ch in lights)
         self.goal_jolts = tuple(map(int, req.split(',')))
 
-    def neighbors(self, state):
-        for bs in self.buttons:
-            s = list(state)
-            for b in bs:
-                s[b] = not s[b]
-            yield tuple(s)
 
-    def solve_part1(self):
-        q = [(0, self.state)]
-        seen = {self.state}
-        while q:
-            cost, s = q.pop(0)
-            if s == self.goal:
-                return cost
+def solve_part1(buttons, goal):
+    q = [(0, (False, ) * len(goal))]
+    seen = {q[0][1]}
+    while q:
+        cost, lights = q.pop(0)
+        if lights == goal:
+            return cost
+        for n in neighbors(buttons, lights):
+            if n not in seen:
+                seen.add(n)
+                q.append((cost + 1, n))
+    assert False
 
-            for n in self.neighbors(s):
-                if n not in seen:
-                    seen.add(n)
-                    q.append((cost + 1, n))
 
-        assert False
+def solve_part2(buttons, goal):
+    presses = [Int(f'presses{i}') for i in range(len(buttons))]
 
-    def solve_part2(self):
-        n = len(self.buttons)
-        nj = len(self.goal_jolts)
-        presses = [Int(f'presses{i}') for i in range(n)]
-        outputs = [Int(f'o{i}') for i in range(nj)]
+    s = Optimize()
+    for p in presses:
+        s.add(p >= 0)
 
-        s = Optimize()
-        for p in presses:
-            s.add(p >= 0)
+    output_accs = {i: 0 for i in range(len(goal))}
 
-        output_accs = {i: 0 for i in range(nj)}
+    for p, bs in zip(presses, buttons):
+        for b in bs:
+            output_accs[b] += p
 
-        for bidx, bs in enumerate(self.buttons):
-            for b in bs:
-                output_accs[b] += presses[bidx]
+    for i in range(len(goal)):
+        s.add(output_accs[i] == goal[i])
 
-        for i, o in enumerate(outputs):
-            s.add(o == self.goal_jolts[i])
-            s.add(o == output_accs[i])
+    s.minimize(sum(presses))
 
-        s.minimize(sum(presses))
+    assert s.check() == sat
 
-        assert s.check() == sat
-
-        m = s.model()
-        return sum(m[p].as_long() for p in presses)
+    m = s.model()
+    return sum(m[p].as_long() for p in presses)
 
 
 a1 = a2 = 0
 for line in lines:
     m = Machine(line)
-    a1 += m.solve_part1()
-    a2 += m.solve_part2()
+    a1 += solve_part1(m.buttons, m.goal_lights)
+    a2 += solve_part2(m.buttons, m.goal_jolts)
 
 print('part1:', a1)
 print('part2:', a2)
