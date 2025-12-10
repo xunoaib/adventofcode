71d922a3249c28d587356c6c61bb04296b317663
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-10 00:45:24 -0500

    Solve part 2

--- 2025/day10/day10.py
+++ 2025/day10/day10.py
@@ -4,6 +4,8 @@ from collections import Counter, defaultdict
 from heapq import heappop, heappush
 from itertools import pairwise, permutations, product
 
+from z3 import Int, Optimize, Solver, sat
+
 aa = bb = None
 
 s = sys.stdin.read()
@@ -57,7 +59,7 @@ class Machine2:
         q = [(0, self.jolts)]
         seen = {q[0][1]}
         while q:
-            cost, jolts = heappop(q)
+            cost, jolts = q.pop(0)
 
             if jolts == self.goal_jolts:
                 return cost
@@ -65,22 +67,66 @@ class Machine2:
             for n in self.neighbors(jolts):
                 if n not in seen:
                     seen.add(n)
-                    heappush(q, (cost + 1, n))
+                    q.append((cost + 1, n))
 
         assert False
+
+    def solvez(self):
+        n = len(self.buttons)
+        nj = len(self.goal_jolts)
+        presses = [Int(f'presses{i}') for i in range(n)]
+        outputs = [Int(f'o{i}') for i in range(nj)]
+
+        s = Optimize()
+        for p in presses:
+            s.add(p >= 0)
+
+        output_accs = {i: 0 for i in range(nj)}
+
+        for bidx, bs in enumerate(self.buttons):
+            for b in bs:
+                output_accs[b] += presses[bidx]
+
+        for i, o in enumerate(outputs):
+            s.add(o == self.goal_jolts[i])
+            s.add(o == output_accs[i])
+
+        s.minimize(sum(presses))
+
+        assert s.check() == sat
+
+        m = s.model()
+        return sum(m[p].as_long() for p in presses)
+
+        # button1 = (2,3,4,5,6,7,8)
+        # button2 = (0,1,2,3,7)
+        # button3 = (1,2,3,4,8)
+        # button4 = (1,2,3,4,5,6,8)
+        # button5 = (0,4,6,7,8)
+        # button6 = (4,6,7)
+        # button7 = (5)
+        # button8 = (0,1,3,7,8)
+        # totals  = {13,30,32,41,36,25,29,24,45}
+
+        # for p, o in zip(presses, outputs):
+        #     s.add(o == p)
 
     def neighbors(self, jolts):
         for bs in self.buttons:
             njolts = list(jolts)
             for b in bs:
                 njolts[b] += 1
-            yield tuple(njolts)
+                if njolts[b] > self.goal_jolts[b]:
+                    break
+            else:
+                yield tuple(njolts)
 
 
 aa = bb = 0
 for line in lines:
-    aa += Machine(line).solve()
-    x = Machine2(line).solve()
+    # aa += Machine(line).solve()
+    # x = Machine2(line).solve()
+    x = Machine2(line).solvez()
     print(x)
     bb += x
 
