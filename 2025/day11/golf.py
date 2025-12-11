f910b499beca2e83242f0f0d791fcfa15e44e56e
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-11 18:39:38 -0500

    Golf 244 bytes

--- 2025/day11/golf.py
+++ 2025/day11/golf.py
@@ -2,8 +2,5 @@ from functools import cache
 o='out'
 p=lambda c:[sum(map(p,g[c])),1][c==o]
 q=cache(lambda c,s:[sum(q(n,s|(n=='fft')|(n=='dac')*2)for n in g[c]),s>2][c==o])
-g={o:[]}
-for l in open(0):
-	a,*b=l[:-1].split()
-	g[a[:-1]]=set(b)
+g={a[:-1]:set(b)for a,*b in(l.split()for l in open(0))}|{o:[]}
 print(p('you'),q('svr',0))
