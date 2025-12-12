a50ef0a11fc062dc505aff34aba650bd4ffbf13e
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-12 04:01:12 -0500

    Clean up day 12

--- 2025/day12/day12.py
+++ 2025/day12/day12.py
@@ -1,24 +1,13 @@
 import sys
 
-*gs, end = sys.stdin.read().strip().split('\n\n')
-shapes = []
-
-for g in gs:
-    lines = g.split('\n')[1:]
-    shapes.append(
-        {
-            (r, c)
-            for r, line in enumerate(lines)
-            for c, ch in enumerate(line) if ch == '#'
-        }
-    )
+*s, r = sys.stdin.read().strip().split('\n\n')
+shapes = [g.count('#') for g in s]
 
 a1 = 0
-for line in end.split('\n'):
+for line in r.split('\n'):
     a, *counts = line.split()
     w, l = map(int, a[:-1].split('x'))
     counts = map(int, counts)
-    size = sum(count * len(shape) for count, shape in zip(counts, shapes))
-    a1 += size <= w * l
+    a1 += w * l >= sum(count * shape for count, shape in zip(counts, shapes))
 
 print('part1:', a1)
