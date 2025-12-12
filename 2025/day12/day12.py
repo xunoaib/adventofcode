f149a30d9c9963d98dd20141da633d0c3aa945a9
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-12 16:45:53 -0500

    Clean up day 12

--- 2025/day12/day12.py
+++ 2025/day12/day12.py
@@ -1,10 +1,10 @@
 import re
 
-r = open(0).read().strip().split('\n\n')[-1]
+s = open(0).read().strip().split('\n\n')[-1]
 a1 = 0
 
-for l in r.split('\n'):
+for l in s.split('\n'):
     w, l, *c = map(int, re.findall(r'\d+', l))
-    a1 += w * l >= sum(v * 9 for v in c)
+    a1 += w * l >= sum(c) * 9
 
 print('part1:', a1)
