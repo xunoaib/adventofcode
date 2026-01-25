138b8e0c18a94bf6f3a55c9f313e9f2c193a4d9e
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-01-25 04:31:01 -0500

    Solve part 1

--- 2016/day12/day12.py
+++ 2016/day12/day12.py
@@ -3,27 +3,57 @@ import sys
 
 class CPU:
 
-    def __init__(self):
-        self.registers = [0] * 4
+    def __init__(self, mem: list[str]):
+        self.pc = 0
+        self.registers = dict(zip('abcd', [0] * 4))
+        self.mem = mem
 
-    def execute(self, inst: str):
-        args = inst.split()
-        print(repr(inst))
+    def step(self):
+        if self.pc >= len(self.mem):
+            return False
+
+        print(self.pc, self.registers, self.mem[self.pc])
+
+        args = self.mem[self.pc].split()
+
         match args:
             case ['cpy', x, y]:
-                self.registers[ord(y) - ord('a')] = int(x) if x.isdigit(
-                ) else self.registers[ord(x) - ord('a')]
+                self.registers[y] = self.parsevalue(x)
+                self.pc += 1
+            case ['inc', x]:
+                self.registers[x] += 1
+                self.pc += 1
+            case ['dec', x]:
+                self.registers[x] -= 1
+                self.pc += 1
+            case ['jnz', x, y]:
+                if self.parsevalue(x):
+                    self.pc += self.parsevalue(y)
+                else:
+                    self.pc += 1
             case _:
                 print('unknown instruction')
 
+        return self.pc < len(self.mem)
+
+    def run(self):
+        while self.step():
+            pass
+
+    def parsevalue(self, v: str):
+        try:
+            return int(v)
+        except ValueError:
+            return self.registers[v]
+
 
 def main():
-    cpu = CPU()
-
     lines = sys.stdin.read().strip().splitlines()
+    cpu = CPU(lines)
+    cpu.run()
 
-    for line in lines:
-        cpu.execute(line)
+    print(cpu.registers)
+    print('part1:', cpu.registers['a'])
 
 
 if __name__ == '__main__':
