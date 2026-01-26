f3ab85fb38f278c83299fe039052019511824195
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-01-26 16:43:12 -0500

    Refactor day 14

--- 2016/day14/day14.py
+++ 2016/day14/day14.py
@@ -1,8 +1,6 @@
 from functools import cache
 from hashlib import md5
 
-aa = bb = None
-
 
 @cache
 def get_nlet(h: str, n: int):
@@ -14,64 +12,38 @@ def get_nlet(h: str, n: int):
 
 
 @cache
-def gen_hash(idx):
+def hash1(idx):
     return md5(f'{salt}{idx}'.encode()).hexdigest()
 
 
 @cache
-def gen_hash2(idx):
+def hash2(idx):
     h = md5(f'{salt}{idx}'.encode()).hexdigest()
     for _ in range(2016):
         h = md5(h.encode()).hexdigest()
     return h
 
 
-def part1():
+def solve(hash_func):
     keys = []
-
     i = 0
     while len(keys) < 64:
-        h = gen_hash(i)
-        if v := get_nlet(h, 3):
-            for j in range(i + 1, i + 1000):
-                g = gen_hash(j)
-                if v[0] * 5 in g:
-                    # print(f'found key {i} {j} : {h} {g}')
-                    keys.append(i)
-                    break
-        i += 1
-
-    return str(keys[-1])
-
-
-def part2():
-    keys = []
-
-    i = 0
-    while len(keys) < 64:
-        h = gen_hash2(i)
-        if v := get_nlet(h, 3):
+        if v := get_nlet(hash_func(i), 3):
             for j in range(i + 1, i + 1001):
-                g = gen_hash2(j)
-                if v[0] * 5 in g:
-                    print(f'found key {i} {j} : {h} {g}')
+                if v[0] * 5 in hash_func(j):
                     keys.append(i)
                     break
         i += 1
-
-    return str(keys[-1])
+    return keys[-1]
 
 
 salt = input()
 
-# aa = part1()
-bb = part2()
+a1 = solve(hash1)
+print('part1:', a1)
 
-if locals().get('aa') is not None:
-    print('part1:', aa)
+a2 = solve(hash2)
+print('part2:', a2)
 
-if locals().get('bb') is not None:
-    print('part2:', bb)
-
-# assert aa == 0
-# assert bb == 0
+assert a1 == 15035
+assert a2 == 19968
