a09664f54b585a68764a40a36712b4920ecb4939
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-01-26 00:22:40 -0500

    Solve part 2

--- 2016/day13/day13.py
+++ 2016/day13/day13.py
@@ -1,4 +1,6 @@
 def is_open(x, y):
+    if x < 0 or y < 0:
+        return False
     v = N + x * x + 3 * x + 2 * x * y + y + y * y
     return f'{v:b}'.count('1') % 2 == 0
 
@@ -7,7 +9,7 @@ def neighbors4(r, c):
     return {(r + dr, c + dc) for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]}
 
 
-def bfs(src, tar):
+def part1(src, tar):
     q = [(0, src)]
     seen = {src}
     while q:
@@ -18,23 +20,34 @@ def bfs(src, tar):
             if n not in seen and is_open(*n):
                 q.append((cost + 1, n))
                 seen.add(n)
-    assert False
 
 
-aa = bb = None
+def part2(src, steps):
+    assert is_open(*src)
+    q = [(0, src)]
+    seen = {src: 0}
+    while q:
+        cost, p = q.pop(0)
+        if cost > steps:
+            break
+        for n in neighbors4(*p):
+            if n not in seen and is_open(*n):
+                q.append((cost + 1, n))
+                seen[n] = cost + 1
+
+    return sum(d <= steps for d in seen.values())
+
 
 N = int(input())
 
 src = (1, 1)
 tar = (31, 39)
 
-aa = bfs(src, tar)
+a1 = part1(src, tar)
+a2 = part2(src, 50)
 
-if locals().get('aa') is not None:
-    print('part1:', aa)
+print('part1:', a1)
+print('part2:', a2)
 
-if locals().get('bb') is not None:
-    print('part2:', bb)
-
-assert aa == 90
-# assert bb == 0
+assert a1 == 90
+assert a2 == 135
