19289d34961b3691c9eb3f6b2e9c5be9ca369448
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-02-02 00:36:29 -0500

    541

--- 2016/day10/day10.py
+++ 2016/day10/day10.py
@@ -10,11 +10,7 @@ f=lambda V:len(V)>1and all(isinstance(v,T)for v in V)
 q=[i for i in I.items()if f(i[1])]
 while q:
 	n,v=q.pop()
-	for v,o in zip(sorted(v),O[n]):
-		I[o][I[o].index(n)]=v
-		q+=[(o,I[o])]*f(I[o])
-print(a:=next(T(k[4:])for k,v in I.items()if set(v)=={61,17}))
+	for v,o in zip(sorted(v),O[n]):I[o][I[o].index(n)]=v;q+=[(o,I[o])]*f(I[o])
+print(next(T(k[4:])for k,v in I.items()if set(v)=={61,17}))
 x,y,z=(I[f'output {o}'][0]for o in'012')
-print(b:=x*y*z)
-assert a == 118
-assert b == 143153
+print(x*y*z)
