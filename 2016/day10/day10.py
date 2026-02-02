57938851f59153f74e008df9bcb93403f66f2523
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-02-02 00:26:25 -0500

    623

--- 2016/day10/day10.py
+++ 2016/day10/day10.py
@@ -1,37 +1,30 @@
-import math
 import re
-
-O = {}
-I = {}
-
+O={}
+I={}
 for L in open(0):
-    if m := re.match(r'^value (.*) goes to (.*)$', L):
-        s, t = m.groups()
-        O[int(s)] = t
-        I[t] = I.get(t, []) + [int(s)]
-    elif m := re.match(r'^(.*) gives low to (.*) and high to (.*)$', L):
-        s, l, h = m.groups()
-        O[s] = [l, h]
-        I |= {k: I.get(k, []) + [s] for k in [l, h]}
-
+	if m:=re.search(r'e (.*) g.*o (.*)',L):
+		s,t=m.groups()
+		O[int(s)]=t
+		I[t]=I.get(t,[])+[int(s)]
+	elif m:=re.search(r'(.*) g.*o (.*) a.*o (.*)',L):
+		s,l,h=m.groups()
+		O[s]=[l,h]
+		I|={k:I.get(k,[])+[s]for k in[l,h]}
 
 def f(V):
-    return len(V) == 2 and all(isinstance(v, int) for v in V)
+	return len(V)>1and all(isinstance(v,int)for v in V)
 
-
-q = [(n, v) for n, v in I.items() if f(v)]
+q=[(n,v)for n,v in I.items()if f(v)]
 
 while q:
-    n, v = q.pop()
-    for v, o in zip(sorted(v), O[n]):
-        I[o][I[o].index(n)] = v
-        q += [(o, I[o])] * f(I[o])
+	n,v=q.pop()
+	for v,o in zip(sorted(v),O[n]):
+		I[o][I[o].index(n)]=v
+		q+=[(o,I[o])]*f(I[o])
 
-a = next(int(k[4:]) for k, v in I.items() if set(v) == {61, 17})
-b = math.prod(I[f'output {o}'][0] for o in '012')
-
-print(a)
-print(b)
+print(a:=next(int(k[4:])for k,v in I.items()if set(v)=={61,17}))
+x,y,z=(I[f'output {o}'][0]for o in'012')
+print(b:=x*y*z)
 
 assert a == 118
 assert b == 143153
