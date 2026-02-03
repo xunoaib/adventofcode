1f765227d1e046b231c58370c12bf38d108d7ed5
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-02-03 02:36:17 -0500

    Golf Day 10, 2016 (506 bytes)

--- 2016/day10/day10-golf.py
+++ 2016/day10/day10-golf.py
@@ -8,8 +8,7 @@ for L in open(0):
 	elif m:=R('(.*) g.*o (.*) a.*o (.*)',L):s,l,h=m.groups();O[s]=[l,h];I|={k:I.get(k,[])+[s]for k in[l,h]}
 f=lambda V:len(V)>1and all(isinstance(v,T)for v in V)
 q=[i for i in I.items()if f(i[1])]
-while q:
-	n,v=q.pop()
-	for v,o in zip(sorted(v),O[n]):I[o][I[o].index(n)]=v;q+=[(o,I[o])]*f(I[o])
-x,y,z=(I[f'output '+o][0]for o in'012')
-print(next(T(k[4:])for k,v in I.items()if set(v)=={61,17}),x*y*z)
+while q:n,v=q.pop();if set(v)=={61,17}:print(n[:4:]);for v,o in zip(sorted(v),O[n]):I[o][I[o].index(n)]=v;q+=[(o,I[o])]*f(I[o])
+x=1
+for o in'012':x*=I[f'output '+o][0]
+print(x)
