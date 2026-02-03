ea3b1487e721287c4fe29768796c0714ce5aa983
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-02-03 00:55:50 -0500

    533

--- 2016/day10/day10.py
+++ 2016/day10/day10.py
@@ -11,6 +11,5 @@ q=[i for i in I.items()if f(i[1])]
 while q:
 	n,v=q.pop()
 	for v,o in zip(sorted(v),O[n]):I[o][I[o].index(n)]=v;q+=[(o,I[o])]*f(I[o])
-print(next(T(k[4:])for k,v in I.items()if set(v)=={61,17}))
-x,y,z=(I[f'output {o}'][0]for o in'012')
-print(x*y*z)
+x,y,z=(I[f'output '+o][0]for o in'012')
+print(next(T(k[4:])for k,v in I.items()if set(v)=={61,17}),x*y*z)
