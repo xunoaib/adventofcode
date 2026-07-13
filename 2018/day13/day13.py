bac5d96b690e6d928aacbb653d5c9d2548fec2b0
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-07-13 18:19:10 -0400

    WIP: Advance cart and navigate intersections

--- 2018/day13/day13.py
+++ 2018/day13/day13.py
@@ -1,6 +1,13 @@
 import sys
 from collections import defaultdict
 
+# TODO:
+# - identify track sections (top, right, bottom, left) and "facing" directions on each section
+
+DIRS4 = R, D, L, U = (0, 1), (1, 0), (0, -1), (-1, 0)
+
+FACING_R, FACING_D, FACING_L, FACING_U = range(4)
+
 
 def find_in_dir(r: int, c: int, dr: int, dc: int, src: str, tar: str):
     assert grid[r, c] == src
@@ -24,6 +31,24 @@ def find_down(r, c):
 
 def find_up(r, c):
     return find_in_dir(r, c, -1, 0, '\\', '/')
+
+
+def get_track_dir(
+    pos: tuple[int, int],
+    true_facing_dir: int,
+    track: list[tuple[int, int]],
+):
+    section = track.index(pos) // (len(track) // 4)
+    return {
+        (0, FACING_R): 1,
+        (0, FACING_L): -1,
+        (1, FACING_D): 1,
+        (1, FACING_U): -1,
+        (2, FACING_R): -1,
+        (2, FACING_L): 1,
+        (3, FACING_D): -1,
+        (3, FACING_U): 1,
+    }[section, true_facing_dir]
 
 
 def points_between(p1, p2, include_last=False):
@@ -72,8 +97,8 @@ for track_id, src in enumerate(ul_corners):
     dirs = '>v<^'
     points = []
 
-    for f, d in zip(funcs, dirs):
-        tar = f(*src)
+    for next_corner, dir_char in zip(funcs, dirs):
+        tar = next_corner(*src)
         add = points_between(src, tar)
 
         for p in add:
@@ -81,24 +106,56 @@ for track_id, src in enumerate(ul_corners):
             tile_track_ids[p].append(track_id)
 
             # identify cart
-            if grid[p] in dirs:
-                cart_dir = 1 if grid[p] == d else -1
-                carts.append((p, cart_dir, track_id))
-                print('cart at', p)
+            if (cart_char := grid[p]) in dirs:
+                track_dir = 1 if cart_char == dir_char else -1
+                turn_state = -1  # next turn dir (left)
+                facing_dir = dirs.index(cart_char)
+                carts.append((p, track_dir, track_id, turn_state, facing_dir))
 
         points += add
         src = tar
 
     tracks.append(points)
 
-print(carts)
 
-# step carts foward
-for pos, dir, track_id in carts:
-    route = tracks[track_id]
-    i = route.index(pos)
-    npos = route[(i + dir) % len(route)]
-    print(pos, npos)
+def step_cart():
+    print('\n=== STEP ===\n')
+    global carts
+
+    new_carts = []
+
+    # step carts foward
+    for pos, track_dir, track_id, turn_state, facing_dir in carts:
+        track = tracks[track_id]
+        i = track.index(pos)
+        npos = track[(i + track_dir) % len(track)]
+        print('>>> track', track_id, pos, '->', npos, 'tracks:', tile_track_ids[npos])
+
+        if grid[npos] == '+':
+            print('--- intersection ---')
+            if turn_state:  # left/right turn, swap tracks
+                print('--- switching tracks ---')
+                track_id = next(t for t in tile_track_ids[npos] if t != track_id)
+
+            print('old facing', facing_dir)
+            new_facing_dir = (facing_dir + turn_state) % 4
+            print('new facing', new_facing_dir)
+            # print(DIRS4[new_facing_dir])
+
+            turn_state = (turn_state + 2) % 3 - 1
+            print('new turn state:', turn_state)
+
+            x = get_track_dir(npos, new_facing_dir, tracks[track_id])
+            print('track_dir:', x)
+
+        new_carts.append((npos, track_dir, track_id, turn_state, facing_dir))
+
+    carts = new_carts
+
+
+step_cart()
+step_cart()
+
 
 if locals().get('aa') is not None:
     print('part1:', aa)
