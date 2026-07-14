692bbe05c625c81ffd2961b1a60fcf0dea671a13
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-07-14 19:06:05 -0400

    Improve runtime from 15s to 5s

--- 2018/day14/day14.py
+++ 2018/day14/day14.py
@@ -1,33 +1,38 @@
-aa = bb = None
+def step(i, j):
+    s = x[i] + x[j]
+    if s >= 10:
+        x.append(1)
+        x.append(s - 10)
+    else:
+        x.append(s)
+    i = (i + x[i] + 1) % len(x)
+    j = (j + x[j] + 1) % len(x)
+    return i, j
+
 
 n = int(input())
 
-x = [3, 7]
+x = bytearray([3, 7])
 i, j = 0, 1
 
 for _ in range(n):
-    x += [*map(int, str(x[i] + x[j]))]
-    i = (i + x[i] + 1) % len(x)
-    j = (j + x[j] + 1) % len(x)
+    i, j = step(i, j)
 
 while len(x) < n + 10:
-    x += [*map(int, str(x[i] + x[j]))]
-    i = (i + x[i] + 1) % len(x)
-    j = (j + x[j] + 1) % len(x)
+    i, j = step(i, j)
 
-s = ''.join(map(str, x))
+print('part1:', aa := ''.join(map(str, x[n : n + 10])))
 
-print('part1:', aa := s[n : n + 10])
+str_n = str(n)
+str_len_n = len(str_n)
+srch = bytearray(int(c) for c in str_n)
 
-last_idx = 0
-while str(n) not in s:
-    last_idx = len(x) - len(str(n))
-    x += [*map(int, str(x[i] + x[j]))]
-    i = (i + x[i] + 1) % len(x)
-    j = (j + x[j] + 1) % len(x)
-    s = ''.join(map(str, x[last_idx:]))
+idx = x.find(srch)
+while idx == -1:
+    i, j = step(i, j)
+    idx = x.find(srch, len(x) - str_len_n - 2)
 
-print('part2:', bb := last_idx + s.index(str(n)))
+print('part2:', bb := idx)
 
 assert aa == '1031816654'
 assert bb == 20179839
